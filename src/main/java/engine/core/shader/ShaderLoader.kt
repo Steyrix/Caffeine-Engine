86ee14ca196f9a09eb9baca5a510698a88a6a05f
Georgy Pevnev <steyrixio@gmail.com>
2022-01-15 19:11:43 +0300

    Implement shader loader (ShaderLoader.kt)

---
+++ src/main/java/engine/core/shader/ShaderLoader.kt
@@ -0,0 +1,20 @@
+package engine.core.shader
+
+import java.nio.charset.Charset
+import java.nio.file.Files
+import kotlin.io.path.Path
+
+object ShaderLoader {
+    fun loadFromFile(
+        vertexShaderFilePath: String,
+        fragmentShaderFilePath: String
+    ): Shader {
+        return Shader().apply {
+            val encodedVertexShader = Files.readAllBytes(Path(vertexShaderFilePath))
+            val encodedFragmentShader = Files.readAllBytes(Path(fragmentShaderFilePath))
+            val charset = Charset.defaultCharset()
+            createVertexShader(String(encodedVertexShader, charset))
+            createFragmentShader(String(encodedFragmentShader, charset))
+        }
+    }
+}
