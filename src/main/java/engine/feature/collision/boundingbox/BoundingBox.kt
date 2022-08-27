ccf0de49ffff107f8fab6ca8a948cbfbe1107023
Steyrix <steyrixio@gmail.com>
2022-08-27 20:42:42 +0300

    Inherit BoundingBox.kt from Vertexed2D.kt and move all VAO and VBO creation logic there

--- src/main/java/engine/feature/collision/boundingbox/BoundingBox.kt
+++ src/main/java/engine/feature/collision/boundingbox/BoundingBox.kt
@@ -1,38 +1,17 @@
 package engine.feature.collision.boundingbox
 
+import engine.core.render2D.Vertexed2D
 import engine.feature.util.Buffer
-import org.lwjgl.opengl.GL33C.*
-import java.nio.FloatBuffer
-import java.nio.IntBuffer
 
 // TODO: Implement cloneable interface
 // TODO: Implement collider pattern
 open class BoundingBox(
-        posX: Float,
-        posY: Float,
-        width: Float,
-        height: Float,
-) : IntersectableBox(posX, posY, width, height) {
-
-    val vertexBuffer = IntBuffer.allocate(1)
-    val vertexArray = IntBuffer.allocate(1)
-
-    fun initVertexBuffer() {
-        glGenBuffers(IntBuffer.allocate(1))
-
-        val bbVerticesArray = Buffer.RECTANGLE_VERTICES
-        val bbVerticesBuffer = FloatBuffer.wrap(bbVerticesArray)
-
-        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer.get(0))
-        glBufferData(GL_ARRAY_BUFFER, bbVerticesBuffer, GL_STATIC_DRAW)
-    }
-
-    fun initVertexArray() {
-        glGenVertexArrays(vertexArray)
-        glBindVertexArray(vertexArray.get(0))
-
-        glEnableVertexAttribArray(0)
-        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer.get(0))
-        glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, 0)
-    }
-}
+        override var posX: Float,
+        override var posY: Float,
+        override var width: Float,
+        override var height: Float,
+) : IntersectableBox,
+        Vertexed2D(
+                bufferParamsCount = 1,
+                dataArrays = listOf(Buffer.RECTANGLE_VERTICES),
+                verticesCount = 8)
