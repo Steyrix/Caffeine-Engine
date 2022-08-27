ca910738ddbdb992386ffb321226c9c0c73e71e4
georgiypevnev <pevnev.georgy@gmail.com>
2022-08-27 19:51:58 +0300

    Implement default draw method

--- src/main/java/engine/core/render2D/OpenGlObject2D.kt
+++ src/main/java/engine/core/render2D/OpenGlObject2D.kt
@@ -1,6 +1,8 @@
 package engine.core.render2D
 
+import engine.core.shader.Shader
 import engine.core.texture.Texture2D
+import engine.feature.matrix.MatrixComputer
 import org.lwjgl.opengl.GL33C.*
 import java.nio.FloatBuffer
 import java.nio.IntBuffer
@@ -56,8 +58,17 @@ open class OpenGlObject2D(
         }
     }
 
-    fun draw() {
+    fun draw(x: Float, y: Float, xSize: Float, ySize: Float, rotationAngle: Float, shader: Shader) {
+        shader.bind()
 
+        val model = MatrixComputer.getResultMatrix(x, y, xSize, ySize, rotationAngle)
+
+        // TODO: Define texture state
+
+        shader.setUniform(Shader.VAR_KEY_MODEL, model)
+
+        glBindVertexArray(vertexArray.get(0))
+        glDrawArrays(GL_TRIANGLES, 0, verticesCount)
     }
 
     fun dispose() {
