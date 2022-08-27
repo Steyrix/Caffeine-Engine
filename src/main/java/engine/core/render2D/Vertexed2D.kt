bad4a22de1aff3564ab7dab49c78589a485a10f4
Steyrix <steyrixio@gmail.com>
2022-08-27 20:34:41 +0300

    Introduce Vertexed2D.kt class to encapsulate VAO and VBO creationg logic

---
+++ src/main/java/engine/core/render2D/Vertexed2D.kt
@@ -0,0 +1,51 @@
+package engine.core.render2D
+
+import org.lwjgl.opengl.GL33C.*
+import java.nio.FloatBuffer
+import java.nio.IntBuffer
+
+open class Vertexed2D(
+        bufferParamsCount: Int,
+        dataArrays: List<FloatArray>,
+        protected val verticesCount: Int,
+) {
+
+    private var buffersFilled: Int = 0
+    protected val buffers: IntBuffer = IntBuffer.allocate(bufferParamsCount)
+    protected val vertexArray: IntBuffer = IntBuffer.allocate(1)
+
+    private val paramsCount = mutableListOf<Int>()
+
+    init {
+        initBuffers(dataArrays, bufferParamsCount)
+        initVertexArray()
+    }
+
+    private fun initBuffers(
+            dataArrays: List<FloatArray>,
+            bufferParamsCount: Int
+    ) {
+        require(dataArrays.size == bufferParamsCount)
+        glGenBuffers(IntBuffer.allocate(buffers.capacity()))
+
+        dataArrays.forEach {
+            val floatBuffer = FloatBuffer.wrap(it)
+
+            glBindBuffer(GL_ARRAY_BUFFER, buffers.get(buffersFilled++))
+            glBufferData(GL_ARRAY_BUFFER, floatBuffer, GL_STATIC_DRAW)
+
+            paramsCount.add(it.size / verticesCount)
+        }
+    }
+
+    private fun initVertexArray() {
+        glGenVertexArrays(vertexArray)
+        glBindVertexArray(vertexArray.get(0))
+
+        for (attribIndex in 0 until buffersFilled) {
+            glEnableVertexAttribArray(attribIndex)
+            glBindBuffer(GL_ARRAY_BUFFER, buffers.get(attribIndex))
+            glVertexAttribPointer(attribIndex, paramsCount[attribIndex], GL_FLOAT, false, 0, 0)
+        }
+    }
+}
