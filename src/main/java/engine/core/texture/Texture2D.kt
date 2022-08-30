0d1a60aadcd3c3e061b5d40505ffd4ce100c71cd
Steyrix <steyrixio@gmail.com>
2022-08-30 22:05:59 +0300

    Use TextureLoader object method to load texture2D

--- src/main/java/engine/core/texture/Texture2D.kt
+++ src/main/java/engine/core/texture/Texture2D.kt
@@ -12,43 +12,9 @@ class Texture2D(
 
     companion object {
         fun createInstance(src: String): Texture2D {
-            return Texture2D(loadTexture(src))
-        }
-
-        private fun loadTexture(src: String): Int {
-            val width: Int
-            val height: Int
-
-            val buff: ByteBuffer
-
-            stackPush().use {
-                val tWidth = it.mallocInt(1)
-                val tHeight = it.mallocInt(1)
-
-                val channels = it.mallocInt(1)
-
-                buff = stbi_load(src, tWidth, tHeight, channels, 4) ?:
-                        throw Exception("Image file [" + src  + "] not loaded: " + stbi_failure_reason())
-
-                width = tWidth.get()
-                height = tHeight.get()
-            }
-
-            val id: Int = glGenTextures()
-            glBindTexture(id, GL_TEXTURE_2D)
-
-            glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
-
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0,
-                GL_RGBA, GL_UNSIGNED_BYTE, buff)
-            glGenerateMipmap(GL_TEXTURE_2D)
-
-            stbi_image_free(buff)
-
-            return id
+            return Texture2D(TextureLoader.loadTexture(src))
         }
     }
-
 
     fun bind() {
         glBindTexture(id, GL_TEXTURE_2D)
