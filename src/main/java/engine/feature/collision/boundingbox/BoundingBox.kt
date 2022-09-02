11df0e34b676379b498a040acfdf58b566e55197
Steyrix <steyrixio@gmail.com>
2022-09-02 15:03:56 +0300

    Implement draw method for bounding box

--- src/main/java/engine/feature/collision/boundingbox/BoundingBox.kt
+++ src/main/java/engine/feature/collision/boundingbox/BoundingBox.kt
@@ -3,15 +3,17 @@ package engine.feature.collision.boundingbox
 import engine.core.render.Drawable2D
 import engine.core.render.render2D.Vertexed2D
 import engine.core.shader.Shader
+import engine.feature.matrix.MatrixComputer
 import engine.feature.util.Buffer
+import org.lwjgl.opengl.GL33C.*
 
 // TODO: Implement cloneable interface
 // TODO: Implement collider pattern
 open class BoundingBox(
-        override var posX: Float,
-        override var posY: Float,
-        override var width: Float,
-        override var height: Float,
+        override var x: Float,
+        override var y: Float,
+        override var xSize: Float,
+        override var ySize: Float,
 ) : IntersectableBox, Drawable2D,
         Vertexed2D(
                 bufferParamsCount = 1,
@@ -20,12 +22,17 @@ open class BoundingBox(
 
     override var shader: Shader? = null
     override val innerDrawableComponents: MutableList<Drawable2D> = mutableListOf()
-    override var x: Float = 0f
-    override var y: Float = 0f
-    override var xSize: Float = 0f
-    override var ySize: Float = 0f
     override var rotationAngle: Float = 0f
+
     override fun draw2D() {
-        TODO("Not yet implemented")
+        shader?.let {
+            it.bind()
+
+            val model = MatrixComputer.getResultMatrix(x, y, xSize, ySize, rotationAngle)
+            it.setUniform(Shader.VAR_KEY_MODEL, model)
+
+            glBindVertexArray(vertexArrayHandle)
+            glDrawArrays(GL_LINES, 0, verticesCount)
+        }
     }
 }
