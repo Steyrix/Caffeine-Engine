e89870bab926134291160fc9858f78943aa559b0
Steyrix <steyrixio@gmail.com>
2022-09-03 22:14:03 +0300

    Introduce Updatable.kt interface and inherit Updatable2D.kt from it

---
+++ src/main/java/engine/core/update/Updatable.kt
@@ -0,0 +1,5 @@
+package engine.core.update
+
+interface Updatable {
+    fun update(parameters: SetOfParameters)
+}
--- src/main/java/engine/core/update/update2D/Updatable2D.kt
+++ src/main/java/engine/core/update/update2D/Updatable2D.kt
@@ -1,6 +1,9 @@
 package engine.core.update.update2D
 
-interface Updatable2D {
+import engine.core.update.SetOfParameters
+import engine.core.update.Updatable
+
+interface Updatable2D : Updatable {
     var x: Float
     var y: Float
     var xSize: Float
@@ -8,11 +11,13 @@ interface Updatable2D {
     var rotationAngle: Float
 
     // Each component should use its own set of parameters to update itself and its inner components
-    fun update(parameters: SetOf2DParameters) {
-        x = parameters.x
-        y = parameters.y
-        xSize = parameters.xSize
-        ySize = parameters.ySize
-        rotationAngle = parameters.rotationAngle
+    override fun update(parameters: SetOfParameters) {
+        if (parameters is SetOf2DParameters) {
+            x = parameters.x
+            y = parameters.y
+            xSize = parameters.xSize
+            ySize = parameters.ySize
+            rotationAngle = parameters.rotationAngle
+        }
     }
 }
