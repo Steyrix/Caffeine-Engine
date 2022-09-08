6331b3cce06966fd946612ccd6584dd01dd9e9fc
Steyrix <steyrixio@gmail.com>
2022-09-08 14:51:42 +0300

    Move out character map generation logic to TextRendererUtil.kt

--- src/main/java/engine/feature/text/TextRenderer.kt
+++ src/main/java/engine/feature/text/TextRenderer.kt
@@ -1,10 +1,10 @@
 package engine.feature.text
 
-import com.sun.media.sound.InvalidFormatException
 import engine.core.render.render2D.OpenGlObject2D
 import engine.core.shader.Shader
 import engine.core.texture.Texture2D
 import engine.feature.geometry.Point2D
+import engine.feature.text.TextRendererUtil.generateMap
 import engine.feature.util.Buffer
 import java.awt.Dimension
 
@@ -15,10 +15,9 @@ class TextRenderer(
         private var textShader: Shader
 ) {
     companion object {
-        private const val ATLAS_WRONG_FORMAT_ERROR_TEXT = "Invalid texture atlas format"
         private const val NULL_COORDINATES_MAP_ERROR_TEXT = "Character coordinates map is null"
         private const val NULL_ATLAS_ERROR_TEXT = "Texture atlas is null"
-        // TODO: error text to constants, remove exception handling
+
         fun getInstance(
                 charSize: Dimension,
                 textureFilePath: String,
@@ -29,40 +28,6 @@ class TextRenderer(
             val characterCoordinates = generateMap(charSize, textureAtlas, characters)
 
             return TextRenderer(textureAtlas!!, characterCoordinates!!, charSize, initialShader)
-        }
-
-        private fun generateMap(charSize: Dimension,
-                                textureAtlas: Texture2D,
-                                characters: MutableList<Char>): HashMap<Char, Point2D> {
-            val out = HashMap<Char, Point2D>()
-            val xStep: Int
-            val yStep: Int
-            val charsCount: Int
-
-            val widthIsValid = textureAtlas.getWidthF() % charSize.getWidth() == 0.0
-            val heightIsValid = textureAtlas.getHeightF() % charSize.getHeight() == 0.0
-
-            if (widthIsValid && heightIsValid) {
-                xStep = (textureAtlas.getWidthF() / charSize.getWidth()).toInt()
-                yStep = (textureAtlas.getHeightF() / charSize.getHeight()).toInt()
-                charsCount = xStep * yStep
-                if (charsCount != characters.size)
-                    throw InvalidFormatException(ATLAS_WRONG_FORMAT_ERROR_TEXT)
-            } else
-                throw InvalidFormatException(ATLAS_WRONG_FORMAT_ERROR_TEXT)
-
-            var j = 0
-            var k = 0
-
-            for (i in 0 until charsCount) {
-                if (charSize.getWidth() * j >= textureAtlas.getWidthF()) {
-                    j = 0
-                    k++
-                }
-                out[characters[i]] = Point2D(j++.toFloat(), k.toFloat())
-            }
-
-            return out
         }
     }
 
