93ee6baacde95940988d3887abb771348dc7ecb5
Steyrix <steyrixio@gmail.com>
2022-09-08 16:22:20 +0300

    Improve exception message

--- src/main/java/engine/feature/text/TextRendererUtil.kt
+++ src/main/java/engine/feature/text/TextRendererUtil.kt
@@ -8,9 +8,11 @@ import java.util.InvalidPropertiesFormatException
 internal object TextRendererUtil {
     private const val ATLAS_WRONG_FORMAT_ERROR_TEXT = "Invalid texture atlas format"
 
-    fun generateMap(charSize: Dimension,
-                            textureAtlas: Texture2D,
-                            characters: MutableList<Char>): HashMap<Char, Point2D> {
+    fun generateMap(
+            charSize: Dimension,
+            textureAtlas: Texture2D,
+            characters: MutableList<Char>
+    ): HashMap<Char, Point2D> {
         val out = HashMap<Char, Point2D>()
         val xStep: Int
         val yStep: Int
@@ -24,7 +26,11 @@ internal object TextRendererUtil {
             yStep = (textureAtlas.getHeightF() / charSize.getHeight()).toInt()
             charsCount = xStep * yStep
             if (charsCount != characters.size)
-                throw InvalidPropertiesFormatException(ATLAS_WRONG_FORMAT_ERROR_TEXT)
+                throw InvalidPropertiesFormatException(
+                        "$ATLAS_WRONG_FORMAT_ERROR_TEXT: " +
+                                "\n Calculated number of characters = $charsCount" +
+                                "\n Actual number of characters = ${characters.size}"
+                )
         } else
             throw InvalidPropertiesFormatException(ATLAS_WRONG_FORMAT_ERROR_TEXT)
 
