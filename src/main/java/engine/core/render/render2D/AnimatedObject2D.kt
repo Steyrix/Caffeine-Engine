14776d97b4fd1ff1226925fcfd7318193a7c7f17
Steyrix <steyrixio@gmail.com>
2022-09-13 20:19:21 +0300

    Add setter for shader, responsible for calling AnimationHolder2D.kt methods

--- src/main/java/engine/core/render/render2D/AnimatedObject2D.kt
+++ src/main/java/engine/core/render/render2D/AnimatedObject2D.kt
@@ -3,6 +3,7 @@ package engine.core.render.render2D
 import engine.core.shader.Shader
 import engine.core.texture.ArrayTexture2D
 import engine.core.texture.Texture2D
+import engine.core.update.Updatable
 import engine.feature.animation.AnimationHolder2D
 
 class AnimatedObject2D(
@@ -12,20 +13,14 @@ class AnimatedObject2D(
         texture: Texture2D? = null,
         arrayTexture: ArrayTexture2D? = null,
         protected val animationHolder: AnimationHolder2D
-) : OpenGlObject2D(bufferParamsCount, dataArrays, verticesCount, texture, arrayTexture) {
+) : OpenGlObject2D(bufferParamsCount, dataArrays, verticesCount, texture, arrayTexture), Updatable {
 
     override var shader: Shader? = null
-        set(value) {
-            field = value
-            field?.let {
-                animationHolder.updateAnimationUniforms(
-                        target = this,
-                        shader = it
-                )
-            }
-        }
 
-    fun playAnimation(deltaTime: Float) {
+    override fun update(deltaTime: Float) {
+        shader?.let {
+            animationHolder.updateAnimationUniforms(this, it)
+        }
         animationHolder.playAnimation(deltaTime)
     }
 }
