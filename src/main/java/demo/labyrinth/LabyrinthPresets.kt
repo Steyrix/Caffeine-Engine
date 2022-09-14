5383876dabf4ae47257394868858715eb293dc65
Steyrix <steyrixio@gmail.com>
2022-09-14 20:32:09 +0300

    Introduce AnimationKey object

--- src/main/java/demo/labyrinth/LabyrinthPresets.kt
+++ src/main/java/demo/labyrinth/LabyrinthPresets.kt
@@ -2,6 +2,11 @@ package demo.labyrinth
 
 import engine.feature.animation.BasicAnimation
 
+object AnimationKey {
+        const val WALK = "WALK"
+        const val IDLE = "IDLE"
+        const val JUMP = "JUMP"
+}
 data class LabyrinthPresets(
         val characterPresets: CharacterPresets = CharacterPresets()
 )
@@ -12,8 +17,8 @@ data class CharacterPresets(
 
 data class AnimationsPresets(
         val animations: MutableList<BasicAnimation> = mutableListOf(
-                BasicAnimation("IDLE", 1, 0, 1, 1, 100f),
-                BasicAnimation("WALK", 2, 0, 6, 1, 100f),
-                BasicAnimation("JUMP", 3, 0, 3, 1, 200f)
+                BasicAnimation(AnimationKey.IDLE, 1, 0, 1, 1, 100f),
+                BasicAnimation(AnimationKey.WALK, 2, 0, 6, 1, 100f),
+                BasicAnimation(AnimationKey.JUMP, 3, 0, 3, 1, 200f)
         )
 )
