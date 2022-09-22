4cbf479b9d8f3f8c1a3c1f7279dd08807363d454
Steyrix <steyrixio@gmail.com>
2022-09-23 00:01:22 +0300

    Set max light intensity to 3 and draw black dot in the center of light source

--- src/main/resources/shaders/lightingFragmentShader.glsl
+++ src/main/resources/shaders/lightingFragmentShader.glsl
@@ -1,13 +1,18 @@
 #version 330
 in vec2 fragmentUV;
 in vec4 pos;
-layout(origin_upper_left) in vec4 gl_FragCoord;
 out vec4 fColor;
 uniform vec2 lightSourcePos;
 uniform sampler2D textureSample;
 void main(void)
 {
-    float lightIntensity = 1.0/distance(pos.xy, lightSourcePos);
+    float lightIntensity = 1.0/distance(lightSourcePos, pos.xy);
+    if(lightIntensity >= 100) {
+        lightIntensity = 0;
+    }
+    if(lightIntensity >= 3) {
+        lightIntensity = 3;
+    }
     vec4 lightCol = vec4(lightIntensity, lightIntensity, lightIntensity, 1.0);
     fColor = texture(textureSample, fragmentUV).rgba * lightCol;
 
