34f0c4b90f7f9e9b338787d9391df4658cf198f0
Steyrix <steyrixio@gmail.com>
2022-09-23 14:14:04 +0300

    Reworked light source calculating

--- src/main/resources/shaders/lightingFragmentShader.glsl
+++ src/main/resources/shaders/lightingFragmentShader.glsl
@@ -2,10 +2,23 @@
 in vec2 fragmentUV;
 in vec4 pos;
 out vec4 fColor;
-uniform vec2 lightSourcePos;
+
+uniform vec2 screenSize;
+uniform vec2 lightSourceSize;
+uniform vec2 lightSourceCoords;
 uniform sampler2D textureSample;
 void main(void)
 {
+    vec2 screenCenter = vec2(screenSize.x / 2, screenSize.y / 2);
+
+    float lightSourceX =
+    (lightSourceCoords.x - screenCenter.x + lightSourceSize.x) / screenSize.x;
+
+    float lightSourceY =
+    (lightSourceCoords.y - screenCenter.y - lightSourceSize.y) / screenSize.y;
+
+    vec2 lightSourcePos = vec2(lightSourceX, lightSourceY);
+
     float lightIntensity = 1.0/distance(lightSourcePos, pos.xy);
     if(lightIntensity >= 100) {
         lightIntensity = 0;
