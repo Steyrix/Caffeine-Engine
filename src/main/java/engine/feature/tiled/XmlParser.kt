07cc49614695e9bc945d9c8ef5a123216e41952c
Steyrix <steyrixio@gmail.com>
2022-09-28 18:56:59 +0300

    Introduce XmlParser.kt object

---
+++ src/main/java/engine/feature/tiled/XmlParser.kt
@@ -0,0 +1,27 @@
+package engine.feature.tiled
+
+import org.w3c.dom.Document
+import java.io.File
+import javax.xml.parsers.DocumentBuilderFactory
+
+object XmlParser {
+    private const val EXTENSION = "xml"
+    private const val ERROR_MSG_NOT_XML = "The file supplied is not XML!"
+    private const val ERROR_MSG_NOT_FOUND = "The file supplied is not found!"
+    private const val ERROR_MSG_PARSE_ERR = "Unable to parse xml file"
+
+    fun getDocument(xmlFile: File): Document? {
+        if (!xmlFile.exists()) {
+            throw IllegalArgumentException(ERROR_MSG_NOT_FOUND)
+        }
+
+        if (xmlFile.extension != EXTENSION) {
+            throw IllegalArgumentException(ERROR_MSG_NOT_XML)
+        }
+
+        val dbFactory = DocumentBuilderFactory.newInstance()
+        val dBuilder = dbFactory.newDocumentBuilder()
+
+        return dBuilder.parse(xmlFile) ?: throw IllegalAccessError(ERROR_MSG_PARSE_ERR)
+    }
+}
