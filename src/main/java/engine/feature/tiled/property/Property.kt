2b9a32f628a15b349792f98fe9c54f6d2930d400
Steyrix <steyrixio@gmail.com>
2022-10-04 18:14:41 +0300

    Introduce Property.kt sealed class for layers and tiles' properties

---
+++ src/main/java/engine/feature/tiled/property/Property.kt
@@ -0,0 +1,25 @@
+package engine.feature.tiled.property
+
+sealed class Property(
+        val propertyName: String
+)
+
+data class IntProperty(
+        val value: Int,
+        val name: String
+) : Property(name)
+
+data class FloatProperty(
+        val value: Float,
+        val name: String
+) : Property(name)
+
+data class BooleanProperty(
+        val value: Boolean,
+        val name: String
+) : Property(name)
+
+data class StringProperty(
+        val value: String,
+        val name: String
+) : Property(name)
