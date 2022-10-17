019a8e64336f7efeb374f7f072ce8d5e71bf73aa
Steyrix <steyrixio@gmail.com>
2022-10-17 19:06:06 +0300

    Add data class for parameters with velocities

--- src/main/java/engine/core/update/SetOfParameters.kt
+++ src/main/java/engine/core/update/SetOfParameters.kt
@@ -2,10 +2,20 @@ package engine.core.update
 
 interface SetOfParameters
 
-data class SetOf2DParameters(
+data class SetOfStatic2DParameters(
         var x: Float,
         var y: Float,
         var xSize: Float,
         var ySize: Float,
         var rotationAngle: Float
+) : SetOfParameters
+
+data class SetOf2DParametersWithVelocity(
+        var x: Float,
+        var y: Float,
+        var xSize: Float,
+        var ySize: Float,
+        var rotationAngle: Float,
+        var velocityX: Float,
+        var velocityY: Float
 ) : SetOfParameters
