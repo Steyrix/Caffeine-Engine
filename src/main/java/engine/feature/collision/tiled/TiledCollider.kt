da49b50a268f872558eb9de4ad802e4ff5dfdf21
Steyrix <steyrixio@gmail.com>
2022-10-31 23:49:33 +0100

    Introduce TiledCollider.kt

---
+++ src/main/java/engine/feature/collision/tiled/TiledCollider.kt
@@ -0,0 +1,15 @@
+package engine.feature.collision.tiled
+
+import engine.core.entity.Entity
+import engine.feature.collision.Collider
+
+class TiledCollider : Collider {
+
+    override fun reactToCollision() {
+        TODO("Not yet implemented")
+    }
+
+    override fun isColliding(entity: Entity): Boolean {
+        TODO("Not yet implemented")
+    }
+}
