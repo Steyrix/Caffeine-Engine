018c91ab2526e209f36445f0bf957050d6c9fb29
Steyrix <steyrixio@gmail.com>
2022-10-31 23:56:57 +0100

    Implement isColliding method of TiledCollider.kt

--- src/main/java/engine/feature/collision/tiled/TiledCollider.kt
+++ src/main/java/engine/feature/collision/tiled/TiledCollider.kt
@@ -1,15 +1,29 @@
 package engine.feature.collision.tiled
 
 import engine.core.entity.Entity
+import engine.core.update.SetOf2DParametersWithVelocity
 import engine.feature.collision.Collider
+import engine.feature.tiled.TileMap
 
-class TiledCollider : Collider {
+// todo identify which tile is previous
+// todo return to previous tile as reaction to collision
+class TiledCollider(
+        private val parameters: SetOf2DParametersWithVelocity,
+        private val collisionLayerName: String
+) : Collider {
 
     override fun reactToCollision() {
         TODO("Not yet implemented")
     }
 
     override fun isColliding(entity: Entity): Boolean {
-        TODO("Not yet implemented")
+        (entity as? TileMap)?.let {
+            if (it.getTileIndexInLayer(parameters.x, parameters.y, collisionLayerName) != -1) {
+                reactToCollision()
+                return true
+            }
+        }
+
+        return false
     }
 }
