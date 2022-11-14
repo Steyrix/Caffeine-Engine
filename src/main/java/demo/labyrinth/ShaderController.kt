8f195a338064a56c30b8376e324b96437e4cbe91
Steyrix <steyrixio@gmail.com>
2022-11-14 23:18:46 +0100

    Create ShaderController.kt, incapsulate shader creating logic

---
+++ src/main/java/demo/labyrinth/ShaderController.kt
@@ -0,0 +1,46 @@
+package demo.labyrinth
+
+import engine.core.shader.Shader
+import engine.core.shader.ShaderLoader
+import org.joml.Matrix4f
+
+object ShaderController {
+    fun createBoundingBoxShader(projection: Matrix4f): Shader {
+        val vertexPath = this.javaClass.getResource("/shaders/boundingBoxVertexShader.glsl")!!.path
+        val fragmentPath = this.javaClass.getResource("/shaders/boundingBoxFragmentShader.glsl")!!.path
+
+        return ShaderLoader.loadFromFile(
+                vertexShaderFilePath = vertexPath,
+                fragmentShaderFilePath = fragmentPath
+        ).also {
+            it.bind()
+            it.setUniform(Shader.VAR_KEY_PROJECTION, projection)
+        }
+    }
+
+    fun createAnimationShader(projection: Matrix4f): Shader {
+        val vertexPath = this.javaClass.getResource("/shaders/animVertexShader.glsl")!!.path
+        val fragmentPath = this.javaClass.getResource("/shaders/animFragmentShader.glsl")!!.path
+
+        return ShaderLoader.loadFromFile(
+                vertexShaderFilePath = vertexPath,
+                fragmentShaderFilePath = fragmentPath
+        ).also {
+            it.bind()
+            it.setUniform(Shader.VAR_KEY_PROJECTION, projection)
+        }
+    }
+
+    fun createTexturedShader(projection: Matrix4f): Shader {
+        val vertexPath = this.javaClass.getResource("/shaders/texturedVertexShader.glsl")!!.path
+        val fragmentPath = this.javaClass.getResource("/shaders/texturedFragmentShader.glsl")!!.path
+
+        return ShaderLoader.loadFromFile(
+                vertexShaderFilePath = vertexPath,
+                fragmentShaderFilePath = fragmentPath
+        ).also {
+            it.bind()
+            it.setUniform(Shader.VAR_KEY_PROJECTION, projection)
+        }
+    }
+}
