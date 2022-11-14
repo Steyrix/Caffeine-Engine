9ecf9dee46b3141b0616c9f19870ea4e08e044f8
Steyrix <steyrixio@gmail.com>
2022-11-14 17:05:00 +0100

    Add doc

--- src/main/java/demo/labyrinth/LabyrinthVariables.kt
+++ src/main/java/demo/labyrinth/LabyrinthVariables.kt
@@ -1,7 +1,13 @@
 package demo.labyrinth
 
+/* Lighting shaders variables */
+
 var accumulated = 0f
+
 var timeLimit = 0.1f
+
 var lightIntensityCap = 3f
+
 val lightIntensityCaps = listOf(3f, 2.95f, 2.9f)
+
 var current = 0
