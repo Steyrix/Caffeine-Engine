f3d991d865538846c3145ca2fe5e991e98de766c
Steyrix <steyrixio@gmail.com>
2022-12-11 19:46:17 +0400

    Use initialization from LabyrinthInitializer in LabyrinthDemo.kt

--- src/main/java/demo/labyrinth/LabyrinthDemo.kt
+++ src/main/java/demo/labyrinth/LabyrinthDemo.kt
@@ -1,24 +1,11 @@
 package demo.labyrinth
 
-import engine.core.entity.CompositeEntity
-import engine.core.entity.Entity
-import engine.core.render.render2D.AnimatedObject2D
-import engine.core.render.render2D.OpenGlObject2D
 import engine.core.scene.Scene
-import engine.core.shader.Shader
-import engine.core.shader.ShaderLoader
-import engine.core.texture.Texture2D
 import engine.core.window.Window
-import engine.feature.collision.boundingbox.BoundingBox
-import engine.feature.collision.boundingbox.BoundingBoxCollider
 import engine.feature.collision.boundingbox.BoundingBoxCollisionContext
-import engine.feature.collision.tiled.TiledCollider
 import engine.feature.collision.tiled.TiledCollisionContext
-import engine.feature.tiled.parser.TiledResourceParser
 import org.joml.Matrix4f
-import org.joml.Vector2f
 import org.lwjgl.opengl.GL33C.*
-import java.io.File
 
 class LabyrinthDemo(
         override val screenWidth: Float,
@@ -30,7 +17,6 @@ class LabyrinthDemo(
 
     override var renderProjection: Matrix4f? = null
 
-    // todo all initializations to seperate file
     override fun init() {
         renderProjection = Matrix4f().ortho(
                 0f,
@@ -41,186 +27,13 @@ class LabyrinthDemo(
                 1f
         )
 
-        initCharacterGraphics()
-        Character.it = Player(
-                drawableComponent = Character.graphicalComponent!!,
-                params = characterParameters
+        LabyrinthInitializer.initAll(
+                renderProjection!!,
+                screenWidth,
+                screenHeight,
+                bbCollisionContext,
+                tiledCollisionContext
         )
-
-        initCrateGraphics()
-        Crate.it = object : CompositeEntity() {}
-        Crate.it?.addComponent(
-                component = Crate.graphicalComponent as Entity,
-                parameters = crateParameters
-        )
-
-        initCampfireGraphics()
-        Campfire.it = object : CompositeEntity() {}
-        Campfire.it?.addComponent(
-                component = Campfire.graphicalComponent as Entity,
-                parameters = campfireParameters
-        )
-
-        Map.parameters = getMapParameters(screenWidth, screenHeight)
-        initTileMapGraphics()
-        Map.it = object  : CompositeEntity() {}
-        Map.it?.addComponent(
-                component = Map.graphicalComponent as Entity,
-                parameters = Map.parameters
-        )
-
-        initSkeletons()
-        Skeletons.graphicalComponents.forEachIndexed { i, it ->
-            Skeletons.it.add(
-                    CompositeEntity().also { entity ->
-                        entity.addComponent(
-                                component = it,
-                                parameters = skeletonParameters[i]
-                        )
-                    }
-            )
-        }
-
-        initPhysics()
-    }
-
-    private fun initPhysics() {
-        Character.it?.addComponent(
-                Character.boxCollider as Entity,
-                parameters = characterParameters
-        )
-
-        Character.it?.addComponent(
-                Character.tiledCollider as Entity,
-                parameters = characterParameters
-        )
-
-        bbCollisionContext.addEntity(Crate.boundingBox as Entity)
-        tiledCollisionContext.addEntity(Map.graphicalComponent as Entity)
-    }
-
-    private fun initTileMapGraphics() {
-        val vertexShaderPath = this.javaClass.getResource("/shaders/lightingVertexShader.glsl")!!.path
-        val fragmentShaderPath = this.javaClass.getResource("/shaders/lightingFragmentShader.glsl")!!.path
-
-        Map.graphicalComponent = TiledResourceParser.createTileMapFromXml(
-                File(this.javaClass.getResource("/tiled/cave_level.xml")!!.path)
-        )
-        Map.graphicalComponent?.shader = ShaderLoader.loadFromFile(
-                vertexShaderFilePath = vertexShaderPath,
-                fragmentShaderFilePath = fragmentShaderPath
-        ).also {
-            it.bind()
-            it.setUniform(Shader.VAR_KEY_PROJECTION, renderProjection!!)
-
-            it.setUniform("screenSize", Vector2f(screenWidth, screenHeight))
-            it.setUniform("lightSourceSize", Vector2f(campfireParameters.xSize, campfireParameters.ySize))
-            it.setUniform("lightSourceCoords", Vector2f(campfireParameters.x, campfireParameters.y))
-            it.setUniform("lightIntensityCap", lightIntensityCap)
-        }
-
-        Map.parameters.xSize = screenWidth / Map.graphicalComponent!!.relativeWidth
-        Map.parameters.ySize = screenHeight / Map.graphicalComponent!!.relativeHeight
-    }
-
-    private fun initCharacterGraphics() {
-
-        Character.boundingBox = BoundingBox(
-                x = 100f,
-                y = 100f,
-                xSize = 60f,
-                ySize = 60f
-        ).apply {
-            shader = ShaderController.createBoundingBoxShader(renderProjection!!)
-        }
-
-        Character.boxCollider =
-                BoundingBoxCollider(Character.boundingBox!!, characterParameters, bbCollisionContext)
-        Character.tiledCollider =
-                TiledCollider(characterParameters, "Walking Layer", tiledCollisionContext)
-
-        val frameSizeX = 0.1f
-        val frameSizeY = 0.333f
-
-        val texturePath = this.javaClass.getResource("/textures/base_character.png")!!.path
-        Character.graphicalComponent = AnimatedObject2D(
-                frameSizeX,
-                frameSizeY,
-                texture = Texture2D.createInstance(texturePath),
-                animations = characterAnimations
-        ).apply {
-            boundingBox = Character.boundingBox
-            x = 100f
-            y = 100f
-            xSize = 60f
-            ySize = 60f
-            shader = ShaderController.createAnimationShader(renderProjection!!)
-        }
-    }
-
-    private fun initCrateGraphics() {
-        Crate.boundingBox = BoundingBox(
-                x = 400f,
-                y = 150f,
-                xSize = 70f,
-                ySize = 70f
-        ).apply {
-            shader = ShaderController.createBoundingBoxShader(renderProjection!!)
-        }
-
-        val texturePath = this.javaClass.getResource("/textures/obj_crate.png")!!.path
-        Crate.graphicalComponent = OpenGlObject2D(
-                texture2D = Texture2D.createInstance(texturePath),
-        ).apply {
-            boundingBox = Crate.boundingBox
-            shader = ShaderController.createTexturedShader(renderProjection!!)
-        }
-    }
-
-    private fun initCampfireGraphics() {
-        val frameSizeX = 0.2f
-        val frameSizeY = 1.0f
-
-        val texturePath = this.javaClass.getResource("/textures/camp_fire_texture.png")!!.path
-        Campfire.graphicalComponent = AnimatedObject2D(
-                frameSizeX = frameSizeX,
-                frameSizeY = frameSizeY,
-                texture = Texture2D.createInstance(texturePath),
-                animations = campfireAnimations
-        ).apply {
-            shader = ShaderController.createAnimationShader(renderProjection!!)
-        }
-    }
-
-    private fun initSkeletons() {
-        val frameSizeX = 0.1f
-        val frameSizeY = 0.1f
-        val texturePath = this.javaClass.getResource("/textures/base_skeleton.png")!!.path
-
-        for (i in 0..1) {
-            val box = BoundingBox(
-                    x = 150f + (100 * i),
-                    y = 120f,
-                    xSize = 50f,
-                    ySize = 50f,
-                    rotationAngle = 0f
-            ).apply {
-                shader = ShaderController.createBoundingBoxShader(renderProjection!!)
-            }
-            Skeletons.boxes.add(box)
-
-            val skeletonObject = AnimatedObject2D(
-                    frameSizeX = frameSizeX,
-                    frameSizeY = frameSizeY,
-                    texture = Texture2D.createInstance(texturePath),
-                    animations = characterAnimations
-            ).apply {
-                boundingBox = box
-                shader = ShaderController.createAnimationShader(renderProjection!!)
-            }
-
-            Skeletons.graphicalComponents.add(skeletonObject)
-        }
     }
 
     override fun input(window: Window) {
