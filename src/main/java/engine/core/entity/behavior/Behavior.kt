26e7dfafd3f8cdc6be4ae8fa0f1f5bd44fde4422
Steyrix <steyrixio@gmail.com>
2022-12-26 16:16:42 +0400

    Add Float parameter to lambdas in Behavior.kt

--- src/main/java/engine/core/entity/behavior/Behavior.kt
+++ src/main/java/engine/core/entity/behavior/Behavior.kt
@@ -5,10 +5,10 @@ import engine.core.update.SetOfParameters
 sealed class Behavior
 
 data class SimpleBehavior(
-        val parameterChanging: (SetOfParameters) -> Unit,
+        val parameterChanging: (Float, SetOfParameters) -> Unit,
 ) : Behavior()
 
 data class LoopedBehavior(
         val loopCondition: (SetOfParameters) -> Boolean,
-        val parameterChanging: (SetOfParameters) -> Unit,
+        val parameterChanging: (Float, SetOfParameters) -> Unit,
 ) : Behavior()
