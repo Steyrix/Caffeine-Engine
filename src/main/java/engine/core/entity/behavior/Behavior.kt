c8793932d2ede3e77f41f6b6c6a45da9931c15f8
Steyrix <steyrixio@gmail.com>
2022-12-26 15:58:01 +0400

    Introduce Behavior.kt sealed class and data classes for simple and looped behavior

---
+++ src/main/java/engine/core/entity/behavior/Behavior.kt
@@ -0,0 +1,15 @@
+package engine.core.entity.behavior
+
+import engine.core.update.SetOfParameters
+
+sealed class Behavior
+
+data class SimpleBehavior(
+        val parameterChanging: (SetOfParameters) -> Unit,
+) : Behavior()
+
+data class LoopedBehavior(
+        val isLoop: Boolean = false,
+        val loopCondition: (SetOfParameters) -> Boolean,
+        val parameterChanging: (SetOfParameters) -> Unit,
+)
