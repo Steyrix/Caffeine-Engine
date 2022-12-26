3f53f3b26460c4165cd000768cfd7d0df2d4e390
Steyrix <steyrixio@gmail.com>
2022-12-26 16:13:36 +0400

    Implement base methods for BehaviouralEntity.kt

--- src/main/java/engine/core/entity/BehaviouralEntity.kt
+++ src/main/java/engine/core/entity/BehaviouralEntity.kt
@@ -1,8 +1,43 @@
 package engine.core.entity
 
-class BehaviouralEntity : CompositeEntity() {
+import engine.core.entity.behavior.Behavior
+import engine.core.entity.behavior.LoopedBehavior
+import engine.core.entity.behavior.SimpleBehavior
+import engine.core.update.SetOfParameters
+
+class BehaviouralEntity(
+        private var behavior: Behavior
+) : CompositeEntity() {
+
+    fun setBehavior(behavior: Behavior) {
+        this.behavior = behavior
+    }
+
+    override fun update(deltaTime: Float) {
+        super.update(deltaTime)
+        behave(deltaTime)
+    }
 
     private fun behave(deltaTime: Float) {
+        when (behavior) {
+            is SimpleBehavior ->
+                processBehavior(
+                        deltaTime,
+                        false
+                ) { false }
+            is LoopedBehavior ->
+                processBehavior(
+                        deltaTime,
+                        true,
+                        (behavior as LoopedBehavior).loopCondition)
+        }
+    }
 
+    private fun processBehavior(
+            deltaTime: Float,
+            isLoop: Boolean,
+            loopCondition: (SetOfParameters) -> Boolean
+    ) {
+        // TODO: implement
     }
 }
