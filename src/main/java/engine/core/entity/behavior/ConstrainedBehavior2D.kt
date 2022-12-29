4e2fd6b3dc03627d5c1d39fe4dc72096b1d7f936
Steyrix <steyrixio@gmail.com>
2022-12-29 17:04:11 +0400

    Introduce ConstrainedBehavior2D.kt

---
+++ src/main/java/engine/core/entity/behavior/ConstrainedBehavior2D.kt
@@ -0,0 +1,12 @@
+package engine.core.entity.behavior
+
+import engine.core.update.SetOfParameters
+
+class ConstrainedBehavior2D(
+
+) : Behavior {
+
+    override fun execute(deltaTime: Float, params: List<SetOfParameters>) {
+        TODO("Not yet implemented")
+    }
+}
