a6f97b468989297ee28809961e5523ff590d842d
Steyrix <steyrixio@gmail.com>
2022-12-29 18:23:17 +0400

    Introduce Skeleton.kt

---
+++ src/main/java/demo/labyrinth/skeleton/Skeleton.kt
@@ -0,0 +1,42 @@
+package demo.labyrinth.skeleton
+
+import engine.core.controllable.SimpleController2D
+import engine.core.entity.CompositeEntity
+import engine.core.render.render2D.AnimatedObject2D
+import engine.core.update.SetOf2DParametersWithVelocity
+
+class Skeleton(
+        drawableComponent: AnimatedObject2D,
+        params: SetOf2DParametersWithVelocity
+) : CompositeEntity() {
+
+    private val controller = SimpleController2D(
+            params,
+            absVelocityY = 10f,
+            absVelocityX = 10f,
+            modifier = 20f,
+            isControlledByUser = false
+    )
+
+    init {
+        addComponent(
+                component = drawableComponent,
+                parameters = params
+        )
+
+        addComponent(
+                component = controller,
+                parameters = params
+        )
+    }
+
+    override fun update(deltaTime: Float) {
+        super.update(deltaTime)
+
+        components
+                .filter { it.key is AnimatedObject2D }
+                .forEach {
+                    (it.key as AnimatedObject2D).setAnimationByKey(controller.getAnimationKey())
+                }
+    }
+}
