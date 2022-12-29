7771c0d16d9bd5530e91a8221f8df497fe379424
Steyrix <steyrixio@gmail.com>
2022-12-29 17:05:04 +0400

    Add accumulated movements parameters to ConstrainedBehavior2D.kt

--- src/main/java/engine/core/entity/behavior/ConstrainedBehavior2D.kt
+++ src/main/java/engine/core/entity/behavior/ConstrainedBehavior2D.kt
@@ -3,7 +3,8 @@ package engine.core.entity.behavior
 import engine.core.update.SetOfParameters
 
 class ConstrainedBehavior2D(
-
+    private var accumulatedMovementX: Float,
+    private var accumulatedMovementY: Float
 ) : Behavior {
 
     override fun execute(deltaTime: Float, params: List<SetOfParameters>) {
