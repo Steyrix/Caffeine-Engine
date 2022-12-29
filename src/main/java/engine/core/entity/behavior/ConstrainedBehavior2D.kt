8ea95a43916adfd9bb6a74bb240f1d5dcd8a76ec
Steyrix <steyrixio@gmail.com>
2022-12-29 17:30:36 +0400

    Implement execute method of ConstrainedBehavior2D.kt

--- src/main/java/engine/core/entity/behavior/ConstrainedBehavior2D.kt
+++ src/main/java/engine/core/entity/behavior/ConstrainedBehavior2D.kt
@@ -1,13 +1,30 @@
 package engine.core.entity.behavior
 
+import engine.core.update.Behavior2DParameters
+import engine.core.update.SetOf2DParametersWithVelocity
 import engine.core.update.SetOfParameters
+import kotlin.math.abs
 
 class ConstrainedBehavior2D(
-    private var accumulatedMovementX: Float,
-    private var accumulatedMovementY: Float
+        private var horizontalCap: Float,
+        private var verticalCap: Float,
+        private var behaviorParams: Behavior2DParameters
 ) : Behavior {
 
     override fun execute(deltaTime: Float, params: SetOfParameters) {
-        TODO("Not yet implemented")
+        val parameters = params as SetOf2DParametersWithVelocity
+
+        behaviorParams.movementX += deltaTime * parameters.velocityX
+        behaviorParams.movementY += deltaTime * parameters.velocityY
+
+        if (abs(behaviorParams.movementX) >= horizontalCap) {
+            behaviorParams.movementX = 0f
+            parameters.velocityX *= -1
+        }
+
+        if (abs(behaviorParams.movementY) >= verticalCap) {
+            behaviorParams.movementY = 0f
+            parameters.velocityY *= -1
+        }
     }
 }
