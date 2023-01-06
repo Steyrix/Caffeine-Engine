568d7286872a8bb10087429a0d4c55d5434d563d
Steyrix <pevnev.georgy@gmail.com>
2023-01-07 02:24:32 +0400

    Cleanup LabyrinthAnimations.kt

--- src/main/java/demo/labyrinth/data/LabyrinthAnimations.kt
+++ src/main/java/demo/labyrinth/data/LabyrinthAnimations.kt
@@ -14,46 +14,6 @@ object AnimationKey {
     const val BURN = "BURN"
 }
 
-val characterAnimations = mutableListOf(
-        BasicAtlasAnimation(
-                AnimationKey.IDLE_R,
-                1, 0, 1, 1, 0, 0, 1f
-        ),
-        BasicAtlasAnimation(
-                AnimationKey.WALK_R,
-                2, 0, 6, 1, 1, 0, 0.2f
-        ).apply {
-            setFirstPosX(1)
-            setLastPosX(6)
-        },
-        BasicAtlasAnimation(
-                AnimationKey.JUMP_R,
-                3, 0, 3, 1, 7, 1, 0.5f
-        ).apply {
-            setFirstPosX(7)
-            setLastPosX(9)
-        },
-
-        BasicAtlasAnimation(
-                AnimationKey.IDLE_L,
-                4, 0, 1, 1, 9, 3, 1f
-        ),
-        BasicAtlasAnimation(
-                AnimationKey.WALK_L,
-                5, 0, 6, 1, 8, 3, 0.2f
-        ).apply {
-            setFirstPosX(8)
-            setLastPosX(3)
-        },
-        BasicAtlasAnimation(
-                AnimationKey.JUMP_L,
-                6, 0, 3, 1, 0, 4, 0.5f
-        ).apply {
-            setFirstPosX(0)
-            setLastPosX(2)
-        }
-)
-
 val characterAnimations2 = mutableListOf(
         SequenceAtlasAnimation(
                 AnimationKey.IDLE_R,
@@ -87,15 +47,6 @@ val characterAnimations2 = mutableListOf(
         ),
 
 )
-
-
-val campfireAnimations =
-        mutableListOf(
-                BasicAtlasAnimation(
-                        AnimationKey.BURN,
-                        100, 0, 4, 1, 0, 0, 0.1f
-                )
-        )
 
 val campfireAnimations2 = mutableListOf(
         SequenceAtlasAnimation(
