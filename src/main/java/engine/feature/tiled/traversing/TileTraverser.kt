9eb3c98144b3e5dad6fcbf6e02ce7984a6dd051b
Steyrix <steyrixio@gmail.com>
2023-02-21 20:22:46 +0400

    Add constructor and getShortestPath method

---
+++ src/main/java/engine/feature/tiled/traversing/TileTraverser.kt
@@ -0,0 +1,25 @@
+package engine.feature.tiled.traversing
+
+import engine.core.entity.Entity
+import engine.feature.tiled.TileMap
+
+class TileTraverser(
+        map: TileMap,
+        entity: Entity
+) {
+
+
+
+    fun moveToTile(
+            map: TileMap,
+            tileIndex: Int
+    ) {
+
+    }
+
+    fun getShortestPath(): List<Int> {
+        // djikstra
+
+        return emptyList()
+    }
+}
