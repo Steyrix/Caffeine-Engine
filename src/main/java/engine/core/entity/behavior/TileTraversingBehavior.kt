67537f66bb00e5f2276708f3ea767b6c12b96508
Steyrix <steyrixio@gmail.com>
2023-02-27 17:52:13 +0400

    Introduce TileTraversingBehavior.kt

---
+++ src/main/java/engine/core/entity/behavior/TileTraversingBehavior.kt
@@ -0,0 +1,12 @@
+package engine.core.entity.behavior
+
+import engine.core.update.SetOfParameters
+
+class TileTraversingBehavior(
+
+) : Behavior {
+
+    override fun execute(deltaTime: Float, params: SetOfParameters) {
+        TODO("Not yet implemented")
+    }
+}
