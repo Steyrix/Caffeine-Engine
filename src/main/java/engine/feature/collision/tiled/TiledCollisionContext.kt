64bd6a10d9b58843742ec86375949f13ffa60441
Steyrix <steyrixio@gmail.com>
2023-03-21 17:25:34 +0400

    Implement map obtaining method for TiledCollisionContext.kt

--- src/main/java/engine/feature/collision/tiled/TiledCollisionContext.kt
+++ src/main/java/engine/feature/collision/tiled/TiledCollisionContext.kt
@@ -4,6 +4,7 @@ import engine.core.entity.Entity
 import engine.core.update.SetOfParameters
 import engine.feature.collision.Collider
 import engine.feature.collision.CollisionContext
+import engine.feature.tiled.TileMap
 
 class TiledCollisionContext : CollisionContext {
 
@@ -15,5 +16,13 @@ class TiledCollisionContext : CollisionContext {
     override fun addCollider(collider: Collider) {
         if (collider !is TiledCollider) return
         super.addCollider(collider)
+    }
+
+    fun getMap(): TileMap? {
+        entities.forEach {
+            if (it is TileMap) return it
+        }
+
+        return null
     }
 }
