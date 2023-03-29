3609f13ad6fa39a52a9a5c2c995fd60b24fa5d0e
Steyrix <steyrixio@gmail.com>
2023-03-29 23:14:39 +0400

    Introduce TileGraph.kt

---
+++ src/main/java/engine/feature/tiled/traversing/TileGraph.kt
@@ -0,0 +1,20 @@
+package engine.feature.tiled.traversing
+
+class TileGraph(
+        val nodes: Map<Int, List<Int>>
+) {
+    val distinct: MutableSet<Int> = mutableSetOf()
+
+    val distances: MutableMap<Int, Int> = mutableMapOf()
+
+    init {
+        nodes.forEach {
+            distances[it.key] = 1
+            distinct.add(it.key)
+        }
+    }
+
+    fun increaseDistance(target: Int) {
+        distances[target]?.inc()
+    }
+}
