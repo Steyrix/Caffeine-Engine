c7588a44c5cd86cbc573ca04a5a517986593b762
Steyrix <steyrixio@gmail.com>
2023-03-29 23:45:22 +0400

    Implement djikstra shortest path algorithm

--- src/main/java/engine/feature/tiled/traversing/PathFinder.kt
+++ src/main/java/engine/feature/tiled/traversing/PathFinder.kt
@@ -57,4 +57,57 @@ object PathFinder {
 
         return false
     }
+
+    fun pathToByDijkstra(
+            graph: TileGraph,
+            start: Int,
+            destination: Int
+    ): ArrayDeque<Int> {
+        if (graph.nodes[destination].isNullOrEmpty() || graph.nodes[start].isNullOrEmpty()) return ArrayDeque()
+
+        val out = ArrayDeque<Int>()
+        val dist = IntArray(graph.nodes.size) { Int.MAX_VALUE }
+        val pred = IntArray(graph.nodes.size) { -1 }
+
+        if (!dijkstra(graph, start, destination, dist, pred)) return ArrayDeque()
+
+        out.add(destination)
+        var previous = destination
+        while (pred[previous] != -1) {
+            out.addFirst(pred[previous])
+            graph.increaseDistance(pred[previous])
+            previous = pred[previous]
+        }
+
+        return out
+    }
+
+    private fun dijkstra(
+            graph: TileGraph,
+            start: Int,
+            destination: Int,
+            dist: IntArray,
+            pred: IntArray
+    ): Boolean {
+        val queue = mutableSetOf(*graph.distinct.toTypedArray())
+
+        dist[start] = 0
+
+        while (queue.isNotEmpty()) {
+            val node = queue.minByOrNull { dist[it] } ?: 0
+            queue.remove(node)
+
+            if (node == destination) return true
+
+            graph.nodes[node]!!.forEach {
+                val currDistance = dist[node] + (graph.distances[it] ?: 0)
+                if (currDistance < dist[node]) {
+                    dist[node] = currDistance
+                    pred[node] = it
+                }
+            }
+        }
+
+        return false
+    }
 }
