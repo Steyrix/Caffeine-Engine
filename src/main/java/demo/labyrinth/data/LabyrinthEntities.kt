0f90778b515e9ffb8fdb383e999297cad679bf7d
Steyrix <steyrixio@gmail.com>
2023-03-31 14:29:49 +0400

    Move GameObject interface to separate file

--- src/main/java/demo/labyrinth/data/LabyrinthEntities.kt
+++ src/main/java/demo/labyrinth/data/LabyrinthEntities.kt
@@ -1,36 +1,17 @@
 package demo.labyrinth.data
 
+import demo.labyrinth.data.gameobject.GameObject
 import demo.labyrinth.hp.HealthBar
 import engine.core.entity.CompositeEntity
-import engine.core.entity.Entity
 import engine.core.loop.AccumulatedTimeEvent
 import engine.core.render.render2D.AnimatedObject2D
 import engine.core.render.render2D.OpenGlObject2D
 import engine.core.update.SetOf2DParametersWithVelocity
-import engine.core.update.SetOfParameters
 import engine.core.update.SetOfStatic2DParameters
 import engine.feature.collision.boundingbox.BoundingBox
-import engine.feature.collision.boundingbox.BoundingBoxCollider
-import engine.feature.collision.tiled.TiledCollider
 import engine.feature.tiled.TileMap
 import engine.feature.tiled.traversing.TileGraph
 import engine.feature.tiled.traversing.TileTraverser
-
-interface GameObject {
-    var it: CompositeEntity?
-    fun update(deltaTime: Float) {
-        it?.update(deltaTime)
-    }
-
-    fun draw() {
-        it?.draw()
-    }
-
-    fun addComponent(entity: Entity?, params: SetOfParameters) {
-        if (entity == null) return
-        it?.addComponent(entity, params)
-    }
-}
 
 object Campfire : GameObject {
     override var it: CompositeEntity? = null
---
+++ src/main/java/demo/labyrinth/data/gameobject/GameObject.kt
@@ -0,0 +1,21 @@
+package demo.labyrinth.data.gameobject
+
+import engine.core.entity.CompositeEntity
+import engine.core.entity.Entity
+import engine.core.update.SetOfParameters
+
+interface GameObject {
+    var it: CompositeEntity?
+    fun update(deltaTime: Float) {
+        it?.update(deltaTime)
+    }
+
+    fun draw() {
+        it?.draw()
+    }
+
+    fun addComponent(entity: Entity?, params: SetOfParameters) {
+        if (entity == null) return
+        it?.addComponent(entity, params)
+    }
+}
