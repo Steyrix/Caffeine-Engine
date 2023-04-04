3fd995119ae4b63b5df4a68e99b26f1d6c398756
Steyrix <steyrixio@gmail.com>
2023-04-04 21:29:14 +0400

    Introduce PredicateTimeEvent.kt

---
+++ src/main/java/engine/core/loop/PredicateTimeEvent.kt
@@ -0,0 +1,20 @@
+package engine.core.loop
+
+class PredicateTimeEvent (
+        private val timeLimit: Float,
+        private val predicate: () -> Boolean,
+        private val action: (Float) -> Unit
+) {
+    private var accumulatedTime = timeLimit
+
+    fun schedule(deltaTime: Float) {
+        if (predicate.invoke()) {
+            accumulatedTime += deltaTime
+        }
+
+        if (accumulatedTime >= timeLimit) {
+            accumulatedTime = 0f
+            action.invoke(deltaTime)
+        }
+    }
+}
