940b85f32866a7588c70ada4337c4f427cecb374
Steyrix <steyrixio@gmail.com>
2023-04-06 21:47:52 +0400

    Add damage property to AttackInteraction.kt

--- src/main/java/demo/labyrinth/interaction/AttackInteraction.kt
+++ src/main/java/demo/labyrinth/interaction/AttackInteraction.kt
@@ -2,5 +2,6 @@ package demo.labyrinth.interaction
 
 import engine.core.entity.Interaction
 
-class AttackInteraction : Interaction {
-}
+class AttackInteraction(
+        val damage: Float = 0.1f
+) : Interaction
