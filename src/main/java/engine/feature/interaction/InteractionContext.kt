147085e5c44e3dabe0d5da29bb550f215e5d5429
Steyrix <steyrixio@gmail.com>
2023-04-06 23:07:01 +0400

    Make map value type absolutely generic

--- src/main/java/engine/feature/interaction/InteractionContext.kt
+++ src/main/java/engine/feature/interaction/InteractionContext.kt
@@ -2,17 +2,21 @@ package engine.feature.interaction
 
 import engine.core.entity.Entity
 
-interface InteractionContext {
+interface InteractionContext<T> {
     val isInteracting: (target: Entity, agent: Entity) -> Boolean
 
     val agents: MutableList<Entity>
 
-    fun addAgent(entity: Entity) {
+    val paramsMap: MutableMap<Entity, T>
+
+    fun addAgent(entity: Entity, value: T) {
         agents.add(entity)
+        paramsMap[entity] = value
     }
 
     fun removeAgent(entity: Entity) {
         agents.remove(entity)
+        paramsMap.remove(entity)
     }
 
     fun update(deltaTime: Float) {
