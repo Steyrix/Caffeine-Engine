ec0df001d51ea08e686c92079b59b22387f03a93
Steyrix <steyrixio@gmail.com>
2023-04-06 22:01:23 +0400

    Remove hardcoded behavior from BoundingBoxCollider.kt

--- src/main/java/engine/feature/collision/boundingbox/BoundingBoxCollider.kt
+++ src/main/java/engine/feature/collision/boundingbox/BoundingBoxCollider.kt
@@ -10,30 +10,15 @@ class BoundingBoxCollider(
         private val box: BoundingBox,
         private val parameters: SetOf2DParametersWithVelocity,
         override var collisionContext: CollisionContext,
-        private val onCollision: (BoundingBox) -> Unit = {}
+        private val onCollision: (BoundingBox, BoundingBox, SetOf2DParametersWithVelocity) -> Unit
 ) : Collider {
 
     private var intersectedBox: BoundingBox? = null
 
     override fun reactToCollision() {
         intersectedBox?.let {
-            val horizontalDiff = box.getIntersectionWidth(it)
-            val verticalDiff = box.getIntersectionHeight(it)
-
-            if (isHorizontalContact()) {
-                if (horizontalDiff != 0f) {
-                    parameters.x += horizontalDiff + 2
-                    parameters.velocityX = 0f
-                }
-            } else if (isVerticalContact()) {
-                if (verticalDiff != 0f) {
-                    parameters.y += verticalDiff + 2
-                    parameters.velocityY = 0f
-                }
-            }
+            onCollision(it, box, parameters)
         }
-
-        intersectedBox = null
     }
 
     override fun isColliding(entity: Entity): Boolean {
@@ -42,27 +27,10 @@ class BoundingBoxCollider(
         (entity as? BoundingBox)?.let {
             if (box.isIntersecting(it)) {
                 intersectedBox = it
-                onCollision(it)
                 return true
             }
         }
 
         return false
-    }
-
-    private fun isHorizontalContact(): Boolean {
-        val numberForContact = 2
-        intersectedBox?.let {
-            return it.isContainingNumberOf(numberForContact, false, box.getLeftCollisionPoints())
-                    || it.isContainingNumberOf(numberForContact, false, box.getRightCollisionPoints())
-        } ?: return false
-    }
-
-    private fun isVerticalContact(): Boolean {
-        val numberForContact = 2
-        intersectedBox?.let {
-            return it.isContainingNumberOf(numberForContact, false, box.getTopCollisionPoints())
-                    || it.isContainingNumberOf(numberForContact, false, box.getBottomCollisionPoints())
-        } ?: return false
     }
 }
