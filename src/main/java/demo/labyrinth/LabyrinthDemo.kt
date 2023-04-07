39d526b5e28782d889bf7410ec0065938982d272
Steyrix <steyrixio@gmail.com>
2023-04-07 18:02:43 +0400

    Add blenging for textures with alfa-channels

--- src/main/java/demo/labyrinth/LabyrinthDemo.kt
+++ src/main/java/demo/labyrinth/LabyrinthDemo.kt
@@ -58,6 +58,8 @@ class LabyrinthDemo(
     }
 
     override fun render(window: Window) {
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
         glClear(GL_COLOR_BUFFER_BIT)
         glClearColor(0f, 0.5f, 0f, 0.5f)
 
