407c488563d81ce80ba25ee8f5e5a63be536abb7
Steyrix <steyrixio@gmail.com>
2023-04-07 17:21:59 +0400

    Introduce TempSprites object

---
+++ src/main/java/demo/labyrinth/data/gameobject/TempSprites.kt
@@ -0,0 +1,72 @@
+package demo.labyrinth.data.gameobject
+
+import demo.labyrinth.ShaderController
+import demo.labyrinth.data.hitAnimation
+import engine.core.entity.CompositeEntity
+import engine.core.loop.AccumulatedTimeEvent
+import engine.core.render.render2D.AnimatedObject2D
+import engine.core.texture.Texture2D
+import engine.core.update.SetOfStatic2DParameters
+import org.joml.Matrix4f
+
+object TempSprites : GameObject {
+
+    override var it: CompositeEntity? = CompositeEntity()
+
+    private val actions: MutableList<AccumulatedTimeEvent> = mutableListOf()
+
+    private var renderProjection: Matrix4f? = null
+
+    fun init(projection: Matrix4f) {
+        renderProjection = projection
+    }
+
+    fun generateHit(posX: Float, posY: Float) {
+        if (renderProjection == null) return
+
+        val frameSizeX = 0.25f
+        val frameSizeY = 0.25f
+
+        val texturePath = this.javaClass.getResource("/textures/blood_hit.png")!!.path
+        val texture = Texture2D.createInstance(texturePath)
+
+        val graphicalComponent = AnimatedObject2D(
+            frameSizeX,
+            frameSizeY,
+            texture = texture,
+            animations = hitAnimation
+        ).apply {
+            x = posX
+            y = posY
+            xSize = 10f
+            ySize = 10f
+            shader = ShaderController.createAnimationShader(renderProjection!!)
+        }
+
+        val params = SetOfStatic2DParameters(
+                x = posX,
+                y = posY,
+                xSize = 10f,
+                ySize = 10f,
+                rotationAngle = 0f
+        )
+
+        it?.addComponent(graphicalComponent, params)
+        actions.add(
+                AccumulatedTimeEvent(
+                        timeLimit = 1.2f,
+                        action = { _ ->
+                            it?.removeComponent(params)
+                        }
+                )
+        )
+    }
+
+    override fun update(deltaTime: Float) {
+        actions.forEach {
+            it.schedule(deltaTime)
+        }
+    }
+
+
+}
