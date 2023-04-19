80519775252730eb5bd13fec23a60bca21edfe8d
Steyrix <steyrixio@gmail.com>
2023-04-19 22:48:56 +0400

    Rearrange file, introduce constants

--- src/main/java/demo/labyrinth/data/LabyrinthEntitiesParameters.kt
+++ src/main/java/demo/labyrinth/data/LabyrinthEntitiesParameters.kt
@@ -3,6 +3,10 @@ package demo.labyrinth.data
 import engine.core.update.SetOf2DParametersWithVelocity
 import engine.core.update.SetOfStatic2DParameters
 
+const val HUMANIOD_BOX_OFFSET_MODIFIER = 0.2265f
+
+const val HUMANOID_SIZE = 128f
+
 var hpBarPatameters1 = SetOfStatic2DParameters(
         x = 0f,
         y = 0f,
@@ -21,11 +25,29 @@ var characterParameters = SetOf2DParametersWithVelocity(
         velocityY = 0f
 )
 
-const val HUMANIOD_BOX_OFFSET_MODIFIER = 0.2265f
+val goblinParams1 = SetOf2DParametersWithVelocity(
+        x = 150f,
+        y = 120f,
+        xSize = HUMANOID_SIZE,
+        ySize = HUMANOID_SIZE,
+        rotationAngle = 0f,
+        velocityX = 0f,
+        velocityY = 0f
+)
 
-fun getCharacterBoxOffset() = characterParameters.xSize * HUMANIOD_BOX_OFFSET_MODIFIER / 2
+val goblinParams2 = SetOf2DParametersWithVelocity(
+        x = 250f,
+        y = 120f,
+        xSize = HUMANOID_SIZE,
+        ySize = HUMANOID_SIZE,
+        rotationAngle = 0f,
+        velocityX = 0f,
+        velocityY = 0f
+)
 
-fun getCharacterBoxSize() = characterParameters.xSize - getCharacterBoxOffset() * 2
+fun getHumanoidBoxOffset() = HUMANOID_SIZE * HUMANIOD_BOX_OFFSET_MODIFIER / 2
+
+fun getHumanoidBoxSize() = HUMANOID_SIZE - getHumanoidBoxOffset() * 2
 
 val campfireParameters: SetOfStatic2DParameters = SetOfStatic2DParameters(
         900f, 750f, 50f, 50f, 0f
@@ -36,23 +58,3 @@ fun getMapParameters(screenWidth: Float, screenHeight:Float): SetOfStatic2DParam
             x = 0f, y = 0f, xSize = screenWidth, ySize = screenHeight, rotationAngle = 0f
     )
 }
-
-val goblinParams1 = SetOf2DParametersWithVelocity(
-        x = 150f,
-        y = 120f,
-        xSize = 128f,
-        ySize = 128f,
-        rotationAngle = 0f,
-        velocityX = 0f,
-        velocityY = 0f
-)
-
-val goblinParams2 = SetOf2DParametersWithVelocity(
-        x = 250f,
-        y = 120f,
-        xSize = 128f,
-        ySize = 128f,
-        rotationAngle = 0f,
-        velocityX = 0f,
-        velocityY = 0f
-)
