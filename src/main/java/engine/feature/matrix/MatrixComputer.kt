ee4bff847a6d144fb65291290058fec457cc57e1
Steyrix <steyrixio@gmail.com>
2023-04-19 19:10:55 +0400

    Add worldTranslation property to MatrixComputer.kt to apply translation to drawable objects

--- src/main/java/engine/feature/matrix/MatrixComputer.kt
+++ src/main/java/engine/feature/matrix/MatrixComputer.kt
@@ -1,9 +1,12 @@
 package engine.feature.matrix
 
 import org.joml.Matrix4f
+import org.joml.Vector2f
 import org.joml.Vector3f
 
 object MatrixComputer {
+
+    var worldTranslation = Vector2f(0f, 0f)
 
     fun getResultMatrix(
             posX: Float,
@@ -18,7 +21,7 @@ object MatrixComputer {
         // val scaleMatrix = Matrix4f().scaleXY(xSize, ySize)
 
         return model.apply {
-            translate(Vector3f(posX, posY, 0f))
+            translate(Vector3f(posX + worldTranslation.x, posY + worldTranslation.y, 0f))
             rotateZ(rotationAngle)
             scaleXY(xSize, ySize)
         }
