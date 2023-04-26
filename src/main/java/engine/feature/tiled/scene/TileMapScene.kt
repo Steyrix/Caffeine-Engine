eb3f3e021322c698e44d01c9711938e3d769f8c2
Steyrix <steyrixio@gmail.com>
2023-04-26 21:31:53 +0400

    Create abstract scene for tile map based game scenes

---
+++ src/main/java/engine/feature/tiled/scene/TileMapScene.kt
@@ -0,0 +1,32 @@
+package engine.feature.tiled.scene
+
+import engine.core.scene.Scene
+import org.joml.Matrix4f
+
+abstract class TileMapScene : Scene {
+
+    private var tiledMap: TileMapObject? = null
+
+    override fun init() {
+        renderProjection = Matrix4f().ortho(
+                0f,
+                screenWidth,
+                screenHeight,
+                0f,
+                0f,
+                1f
+        )
+
+        tiledMap = initTileMap(
+                renderProjection!!,
+                screenWidth,
+                screenHeight
+        )
+    }
+
+    abstract fun initTileMap(
+            projection: Matrix4f,
+            screenWidth: Float,
+            screenHeight: Float
+    ): TileMapObject
+}
