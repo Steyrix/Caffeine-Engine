e6f7bec8b1a931f7d9ec7d74351c8e1d80693760
Steyrix <steyrixio@gmail.com>
2023-05-07 16:01:39 +0400

    Copy everything from StartMap to NexusMap.kt

--- src/main/java/demo/medieval_game/scene/NexusMap.kt
+++ src/main/java/demo/medieval_game/scene/NexusMap.kt
@@ -1,14 +1,20 @@
 package demo.medieval_game.scene
 
 import demo.medieval_game.data.MapSceneInitializer
+import demo.medieval_game.data.gameobject.Character
+import demo.medieval_game.data.gameobject.TempSpritesHolder
+import demo.medieval_game.data.starting_level.StartMapInitializer
 import demo.medieval_game.data.starting_level.getNexusMapPreset
-import engine.core.scene.GameObject
+import engine.core.loop.AccumulatedTimeEvent
 import engine.core.session.Session
 import engine.core.window.Window
+import engine.feature.collision.boundingbox.BoundingBoxCollisionContext
 import engine.feature.collision.tiled.TiledCollisionContext
+import engine.feature.interaction.BoxInteractionContext
 import engine.feature.tiled.scene.TileMapObject
 import engine.feature.tiled.scene.TileMapScene
 import org.joml.Matrix4f
+import org.lwjgl.opengl.GL33C.*
 
 class NexusMap(
         override val screenWidth: Float,
@@ -19,10 +25,35 @@ class NexusMap(
 
     private val tiledCollisionContext = TiledCollisionContext()
 
+    private val actions: MutableList<AccumulatedTimeEvent> = mutableListOf()
+
+    private var bbCollisionContext: BoundingBoxCollisionContext? = null
+    private var boxInteractionContext: BoxInteractionContext? = null
+
     override fun init(session: Session) {
         if (session !is MedievalGameSession) return
 
         super.init(session)
+
+        bbCollisionContext = session.bbCollisionContext
+        boxInteractionContext = session.boxInteractionContext
+
+        val tempSpritesHolder = gameContext.find { it is TempSpritesHolder } as? TempSpritesHolder
+        val character = gameContext.find { it is Character } as Character
+
+        // TODO: temp
+        tiledMap?.let {
+            val objects = StartMapInitializer.initAll(
+                    renderProjection!!,
+                    bbCollisionContext!!,
+                    boxInteractionContext!!,
+                    tempSpritesHolder!!
+            ) { params -> it.createTraverser(params) }
+
+            gameContext.addAll(objects)
+            character.updateCollisionContext(tiledCollisionContext)
+            character.updateBoundingBox()
+        }
     }
 
     override fun initTileMap(projection: Matrix4f, screenWidth: Float, screenHeight: Float): TileMapObject {
@@ -34,15 +65,38 @@ class NexusMap(
     }
 
     override fun input(window: Window) {
-        TODO("Not yet implemented")
+        gameContext.forEach { it.input(window) }
     }
 
     override fun update(deltaTime: Float) {
-        TODO("Not yet implemented")
+        gameContext.forEach { entity ->
+            entity.update(deltaTime)
+            if (entity.isDisposed()) {
+                actions.add(
+                        AccumulatedTimeEvent(
+                                timeLimit = 10f,
+                                action = { gameContext.remove(entity) },
+                                initialTime = 0f
+                        )
+                )
+            }
+        }
+
+        actions.forEach { it.schedule(deltaTime) }
+
+        super.update(deltaTime)
+        tiledCollisionContext.update()
+        bbCollisionContext?.update()
+        boxInteractionContext?.update()
     }
 
     override fun render(window: Window) {
-        TODO("Not yet implemented")
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glClear(GL_COLOR_BUFFER_BIT)
+        glClearColor(0f, 0.5f, 0f, 0.5f)
+
+        gameContext.forEach { it.draw() }
     }
 
     override fun onSwitch() {
