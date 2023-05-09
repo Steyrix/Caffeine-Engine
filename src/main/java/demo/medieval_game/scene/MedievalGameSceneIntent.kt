0a0f9c69371dd3dbd9a812dbf35a915e22e0e50d
Steyrix <steyrixio@gmail.com>
2023-05-09 15:35:40 +0400

    Create MedievalGameSceneIntent.kt

---
+++ src/main/java/demo/medieval_game/scene/MedievalGameSceneIntent.kt
@@ -0,0 +1,10 @@
+package demo.medieval_game.scene
+
+import engine.core.controllable.Direction
+import engine.core.scene.SceneIntent
+import engine.feature.geometry.Point2D
+
+data class MedievalGameSceneIntent(
+        val direction: Direction,
+        val previousScenePos: Point2D
+): SceneIntent
