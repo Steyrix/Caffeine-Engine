d7c48e8476ccdc066dc1f7e21ddb2a2c87000f30
Steyrix <steyrixio@gmail.com>
2023-05-09 15:17:19 +0400

    Add empty line

--- src/main/java/engine/core/session/Session.kt
+++ src/main/java/engine/core/session/Session.kt
@@ -3,6 +3,7 @@ package engine.core.session
 import engine.core.scene.GameObject
 
 abstract class Session {
+
     protected abstract val persistentGameObjects: MutableList<GameObject>
 
     abstract fun init(presets: SessionPresets)
