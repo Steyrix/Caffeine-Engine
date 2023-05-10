6ec9747bcbfcee8ec3d07186f8db9172452f06e0
Steyrix <steyrixio@gmail.com>
2023-05-10 14:35:15 +0400

    Add ConstantInteractionContext.kt

---
+++ src/main/java/engine/feature/interaction/ConstantInteractionContext.kt
@@ -0,0 +1,12 @@
+package engine.feature.interaction
+
+import engine.core.entity.Entity
+
+class ConstantInteractionContext : InteractionContext<Unit> {
+
+    override val agents: MutableList<Entity> = mutableListOf()
+
+    override val paramsMap: MutableMap<Entity, Unit> = mutableMapOf()
+
+    override val isInteracting = { _: Entity, _: Entity -> true}
+}
