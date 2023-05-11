d29e19d9de5c366a351267a19b05f3a19455c39d
Steyrix <steyrixio@gmail.com>
2023-05-11 20:39:01 +0400

    Remove redundant overriding

--- src/main/java/demo/medieval_game/scene/NexusMap.kt
+++ src/main/java/demo/medieval_game/scene/NexusMap.kt
@@ -39,15 +39,6 @@ class NexusMap(
         }
     }
 
-    override fun render(window: Window) {
-        glEnable(GL_BLEND)
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-        glClear(GL_COLOR_BUFFER_BIT)
-        glClearColor(0f, 0.5f, 0f, 0.5f)
-
-        gameContext.forEach { it.draw() }
-    }
-
     override fun onSwitch(): MedievalGameSceneIntent {
         TODO("Not yet implemented")
     }
