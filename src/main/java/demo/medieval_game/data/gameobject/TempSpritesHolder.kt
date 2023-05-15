2569525fc237e69bcbd1ac0d4679c1fbfa471ea4
Steyrix <steyrixio@gmail.com>
2023-05-15 16:15:01 +0400

    Use single time events and finish method. Rename it to startScreenFading

--- src/main/java/demo/medieval_game/data/gameobject/TempSpritesHolder.kt
+++ src/main/java/demo/medieval_game/data/gameobject/TempSpritesHolder.kt
@@ -4,9 +4,11 @@ import demo.medieval_game.ShaderController
 import demo.medieval_game.data.hitAnimation
 import engine.core.entity.CompositeEntity
 import engine.core.loop.AccumulatedTimeEvent
+import engine.core.loop.SingleTimeEvent
 import engine.core.render.primitive.Rectangle
 import engine.core.render.render2D.AnimatedObject2D
 import engine.core.scene.GameObject
+import engine.core.shader.Shader
 import engine.core.texture.Texture2D
 import engine.core.update.SetOfStatic2DParameters
 import org.joml.Matrix4f
@@ -16,10 +18,13 @@ class TempSpritesHolder : GameObject {
     override var it: CompositeEntity? = CompositeEntity()
 
     private val actions: MutableList<AccumulatedTimeEvent> = mutableListOf()
+    private val updateActions: MutableList<(Float) -> Unit> = mutableListOf()
 
     private var renderProjection: Matrix4f? = null
 
     private var z: Float = -1f
+
+    private var alpha = 0f
 
     fun init(projection: Matrix4f) {
         renderProjection = projection
@@ -55,7 +60,7 @@ class TempSpritesHolder : GameObject {
 
         it?.addComponent(graphicalComponent, params)
         actions.add(
-                AccumulatedTimeEvent(
+                SingleTimeEvent(
                         timeLimit = 0.8f,
                         action = { _ ->
                             it?.removeComponent(graphicalComponent)
@@ -65,27 +70,59 @@ class TempSpritesHolder : GameObject {
         )
     }
 
-    // TODO: set up screen coords
-    fun generateBlackScreen() {
+    fun startScreenFading(
+            screenWidth: Float,
+            screenHeight: Float,
+            onFinish: () -> Unit
+    ) {
+        if (renderProjection == null) return
+
         val params = SetOfStatic2DParameters(
                 x = 0f,
                 y = 0f,
-                xSize = 300f,
-                ySize = 300f,
+                xSize = screenWidth,
+                ySize = screenHeight,
                 rotationAngle = 0f
         )
 
         val graphicalComponent = Rectangle(0f, 0f, 0f).apply {
-            // todo add primitive shader
+            shader = ShaderController.createPrimitiveShader(renderProjection!!)
         }
 
         it?.addComponent(graphicalComponent, params)
+
+        actions.add(
+                SingleTimeEvent(
+                        timeLimit = 1.5f,
+                        action = { _ ->
+                            it?.removeComponent(graphicalComponent)
+                            onFinish.invoke()
+                        },
+                        initialTime = 0f
+                )
+        )
+
+        updateActions.add { deltaTime ->
+            alpha += deltaTime * 0.5f
+            graphicalComponent.shader?.let {
+                it.bind()
+                it.setUniform(Shader.ALPHA, alpha)
+            }
+        }
+
+        z = Float.MAX_VALUE
     }
 
     override fun update(deltaTime: Float) {
         super.update(deltaTime)
+
+        actions.removeIf { it is SingleTimeEvent && it.isFinished }
         actions.forEach {
             it.schedule(deltaTime)
+        }
+
+        updateActions.forEach {
+            it.invoke(deltaTime)
         }
     }
 
