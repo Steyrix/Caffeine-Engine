fd34a7f3fb506e32af95e917a139b3e31611017d
Steyrix <steyrixio@gmail.com>
2023-05-15 15:00:01 +0400

    Add isFinishedParameter

--- src/main/java/engine/core/loop/AccumulatedTimeEvent.kt
+++ src/main/java/engine/core/loop/AccumulatedTimeEvent.kt
@@ -7,12 +7,16 @@ open class AccumulatedTimeEvent(
 ) {
     private var accumulatedTime = initialTime
 
+    var isFinished = false
+        private set
+
     fun schedule(deltaTime: Float) {
         accumulatedTime += deltaTime
 
         if (accumulatedTime >= timeLimit) {
             accumulatedTime = 0f
             action.invoke(deltaTime)
+            isFinished = true
         }
     }
 }
