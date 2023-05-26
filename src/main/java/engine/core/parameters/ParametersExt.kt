502bafe53115ad06e41a8fb3ec2fb2c8916e49aa
Steyrix <pevnev.georgy@gmail.com>
2023-05-26 16:51:43 +0300

    Introduce parameters package and ParametersExt.kt file

---
+++ src/main/java/engine/core/parameters/ParametersExt.kt
@@ -0,0 +1,14 @@
+package engine.core.parameters
+
+import engine.core.update.SetOf2DParametersWithVelocity
+import engine.core.update.SetOfStatic2DParameters
+
+fun SetOf2DParametersWithVelocity.toStatic(): SetOfStatic2DParameters {
+    return SetOfStatic2DParameters(
+            x,
+            y,
+            xSize,
+            ySize,
+            rotationAngle
+    )
+}
