3fb023fe592368444e84bd0e7094d52b9ce5bdfb
Steyrix <steyrixio@gmail.com>
2023-06-04 17:31:53 +0400

    Remove unused import

--- src/main/java/demo/medieval_game/data/gameobject/Campfire.kt
+++ src/main/java/demo/medieval_game/data/gameobject/Campfire.kt
@@ -2,7 +2,6 @@ package demo.medieval_game.data.gameobject
 
 import demo.medieval_game.ShaderController
 import demo.medieval_game.data.campfireAnimations
-import demo.medieval_game.data.campfireParameters
 import engine.core.entity.CompositeEntity
 import engine.core.render.render2D.AnimatedObject2D
 import engine.core.game_object.SingleGameObject
