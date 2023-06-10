790e2fc7c94847da82a641987f25d489c885110a
Steyrix <steyrixio@gmail.com>
2023-06-10 16:08:54 +0400

    Add NPC.kt class

---
+++ src/main/java/demo/medieval_game/data/gameobject/NPC.kt
@@ -0,0 +1,7 @@
+package demo.medieval_game.data.gameobject
+
+// todo dynamic game object
+// encapsulate initializing behavior
+// use controller entity to define interactions and behavior (e.g. PlayableCharacter, Goblin)
+class NPC {
+}
