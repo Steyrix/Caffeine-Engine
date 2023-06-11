1fd9f3ccf4ee86b3a06b65751690a394bd19d892
Steyrix <steyrixio@gmail.com>
2023-06-11 19:09:10 +0400

    Introduce preset data classes for NPC.kt

---
+++ src/main/java/demo/medieval_game/data/gameobject/npc/NpcPreset.kt
@@ -0,0 +1,21 @@
+package demo.medieval_game.data.gameobject.npc
+
+import engine.core.update.SetOfStatic2DParametersWithOffset
+import engine.feature.animation.Animation
+
+data class NpcBoxPreset(
+        val initialParams: SetOfStatic2DParametersWithOffset,
+        val isSizeBoundToHolder: Boolean
+)
+
+data class AnimationPreset(
+        val atlasTexturePath: String,
+        val frameSizeX: Float,
+        val frameSizeY: Float,
+        val animations: MutableList<Animation>
+)
+
+data class NpcPreset(
+        val box: NpcBoxPreset,
+        val animation: AnimationPreset
+)
