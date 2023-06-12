7ca473cb51e0abe8f96da5143066d298395ca1a5
Steyrix <steyrixio@gmail.com>
2023-06-12 17:46:19 +0400

    Introduce and implement HumanoidController.kt for Medieval Game

---
+++ src/main/java/demo/medieval_game/data/gameobject/npc/HumanoidController.kt
@@ -0,0 +1,25 @@
+package demo.medieval_game.data.gameobject.npc
+
+import engine.core.controllable.AnimationController
+import engine.core.controllable.Direction
+
+abstract class HumanoidController : AnimationController {
+
+    protected var direction: Direction = Direction.LEFT
+    protected var isStriking = false
+    protected var isWalking = false
+
+    override fun getAnimationKey(): String {
+        return when {
+            isStriking -> getStrikingAnimation()
+            isWalking -> getWalkingAnimation()
+            else -> getIdleAnimation()
+        }
+    }
+
+    protected abstract fun getStrikingAnimation(): String
+
+    protected abstract fun getWalkingAnimation(): String
+
+    protected abstract fun getIdleAnimation(): String
+}
