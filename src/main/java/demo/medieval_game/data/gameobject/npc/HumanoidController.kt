c6ce23d0418b4c9e448acf6b3279b04e117b5d8f
Steyrix <steyrixio@gmail.com>
2023-06-13 23:27:33 +0400

    Add constants, typealias and protected property to HumanoidController.kt

--- src/main/java/demo/medieval_game/data/gameobject/npc/HumanoidController.kt
+++ src/main/java/demo/medieval_game/data/gameobject/npc/HumanoidController.kt
@@ -2,12 +2,25 @@ package demo.medieval_game.data.gameobject.npc
 
 import engine.core.controllable.AnimationController
 import engine.core.controllable.Direction
+import java.util.EnumMap
+
+private const val IDLE = "IDLE"
+private const val STRIKE = "STRIKE"
+private const val WALK = "WALK"
+
+typealias MultiMap = HashMap<String, MutableMap<Direction, String>>
 
 abstract class HumanoidController : AnimationController {
 
     protected var direction: Direction = Direction.LEFT
     protected var isStriking = false
     protected var isWalking = false
+
+    protected val animationMultiMap: MultiMap = hashMapOf(
+            IDLE to EnumMap(Direction::class.java),
+            STRIKE to EnumMap(Direction::class.java),
+            WALK to EnumMap(Direction::class.java)
+    )
 
     override fun getAnimationKey(): String {
         return when {
