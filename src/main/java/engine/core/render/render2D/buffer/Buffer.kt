db0da509f9eac0101ce87b8cd93721c3eb3c437f
Steyrix <steyrixio@gmail.com>
2023-06-24 18:02:56 +0400

    Introduce Buffer.kt wrapper
    (cherry picked from commit da696d2e3b4344272057ee6b6d255678af4a92de)

---
+++ src/main/java/engine/core/render/render2D/buffer/Buffer.kt
@@ -0,0 +1,45 @@
+package engine.core.render.render2D.buffer
+
+import org.lwjgl.BufferUtils
+import org.lwjgl.opengl.GL15.*
+import java.nio.FloatBuffer
+
+private const val FLOAT_BYTE_SIZE = 2
+
+class Buffer(
+        data: FloatArray,
+        val vertexCount: Int
+) {
+
+    val handle: Int = glGenBuffers()
+
+    init {
+        val floatBuffer = prepareData(data)
+        bind()
+
+        glBufferData(GL_ARRAY_BUFFER, floatBuffer, GL_STATIC_DRAW)
+    }
+
+    fun update(data: FloatArray, offset: Long) {
+        val floatBuffer = prepareData(data)
+        bind()
+
+        glBufferSubData(GL_ARRAY_BUFFER, offset, floatBuffer)
+    }
+
+    fun bind() {
+        glBindBuffer(GL_ARRAY_BUFFER, handle)
+    }
+
+    fun unbind() {
+        glBindBuffer(GL_ARRAY_BUFFER, 0)
+    }
+
+    private fun prepareData(data: FloatArray): FloatBuffer {
+        return BufferUtils.createFloatBuffer(FLOAT_BYTE_SIZE * data.size)
+                .apply {
+                    put(data)
+                    flip()
+                }
+    }
+}
