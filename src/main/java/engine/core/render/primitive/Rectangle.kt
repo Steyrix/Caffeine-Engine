d9441ddb4a1641b89598e0f547708fd3b4a4a6b7
Steyrix <steyrixio@gmail.com>
2023-06-26 19:32:41 +0400

    Change constructor call in Rectangle.kt

--- src/main/java/engine/core/render/primitive/Rectangle.kt
+++ src/main/java/engine/core/render/primitive/Rectangle.kt
@@ -1,14 +1,12 @@
 package engine.core.render.primitive
 
-import engine.core.render.Mesh
 import engine.core.render.Model
 import engine.core.render.util.DefaultBufferData
 
 class Rectangle(r: Float, g: Float, b: Float) : Model(
-        mesh = Mesh(
-                dataArrays = listOf(
-                        DefaultBufferData.RECTANGLE_INDICES, DefaultBufferData.getColorBuffer(r, g, b)
-                ),
-                verticesCount = 6
-        )
+        listOf(
+                DefaultBufferData.RECTANGLE_INDICES,
+                DefaultBufferData.getColorBuffer(r, g, b)
+        ),
+        verticesCount = 6
 )
