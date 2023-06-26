9f29db3d7aaf0185c4620c83a26610c5d5dfb901
Steyrix <steyrixio@gmail.com>
2023-06-26 16:19:36 +0400

    Shader now belongs to Drawable.kt interface

--- src/main/java/engine/core/render/Drawable.kt
+++ src/main/java/engine/core/render/Drawable.kt
@@ -1,5 +1,10 @@
 package engine.core.render
 
+import engine.core.shader.Shader
+
 interface Drawable {
+
+    var shader: Shader?
+
     fun draw()
 }
--- src/main/java/engine/core/render/Drawable2D.kt
+++ src/main/java/engine/core/render/Drawable2D.kt
@@ -1,11 +1,8 @@
 package engine.core.render
 
-import engine.core.shader.Shader
 import engine.core.update.update2D.Parameterized
 
 interface Drawable2D : Drawable, Parameterized {
-
-    var shader: Shader?
 
     val innerDrawableComponents: MutableList<Drawable2D>
     override fun draw() {
