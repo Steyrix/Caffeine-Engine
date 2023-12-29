22cf93faf2f0f886b89714c667186a228731573d
Steyrix <steyrixio@gmail.com>
2023-12-29 17:03:49 +0400

    Start implementing GameContext.kt interface

---
+++ src/main/java/engine/core/scene/GameContext.kt
@@ -0,0 +1,30 @@
+package engine.core.scene
+
+import engine.core.game_object.CompositeGameEntity
+import engine.core.game_object.GameEntity
+
+interface GameContext {
+
+    val entities: MutableList<GameEntity>
+
+    fun add(entity: GameEntity) {
+        entities.add(entity)
+    }
+
+    fun remove(entity: GameEntity) {
+        entities.remove(entity)
+    }
+
+    fun entitiesSortedByLevelZ(): List<GameEntity> {
+        val out = mutableListOf<GameEntity>()
+        entities.forEach {
+            if (it is CompositeGameEntity) {
+                out.addAll(it.getInnerObjects())
+            } else {
+                out.add(it)
+            }
+        }
+
+        return out.sortedBy { it.getZLevel() }
+    }
+}
