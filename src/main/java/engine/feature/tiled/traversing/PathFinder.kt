1f6cc078eaf85d18cbacacb5155d868437090ff8
Steyrix <steyrixio@gmail.com>
2024-01-05 17:54:13 +0400

    Add TODO

--- src/main/java/engine/feature/tiled/traversing/PathFinder.kt
+++ src/main/java/engine/feature/tiled/traversing/PathFinder.kt
@@ -2,6 +2,7 @@ package engine.feature.tiled.traversing
 
 import java.util.PriorityQueue
 
+// TODO: diagonal adjacent tiles can be surrounded by unwalkable tiles. Thus they need to be deleted from path
 internal object PathFinder {
 
     fun pathTo(
