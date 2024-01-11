bf4d5eae78190ae49a14bc8dc5f2259f41052921
Steyrix <steyrixio@gmail.com>
2024-01-11 17:27:48 +0400

    Introduce DebugMesh.kt

---
+++ src/main/java/engine/feature/tiled/data/debug/DebugMesh.kt
@@ -0,0 +1,23 @@
+package engine.feature.tiled.data.debug
+
+import engine.core.render.Drawable
+import engine.core.shader.Shader
+import engine.core.update.SetOfStatic2DParameters
+
+class DebugMesh : Drawable<SetOfStatic2DParameters> {
+
+    override var shader: Shader?
+        get() = TODO("Not yet implemented")
+        set(value) {}
+    override var zLevel: Float
+        get() = TODO("Not yet implemented")
+        set(value) {}
+
+    override fun draw() {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateParameters(parameters: SetOfStatic2DParameters) {
+        TODO("Not yet implemented")
+    }
+}
