e25cb5f3f0a2e8ddfdfed0c9b183718cdbbcce70
Steyrix <steyrixio@gmail.com>
2024-01-14 17:43:20 +0400

    Change animation frame width

--- src/main/java/demo/medieval_game/data/gameobject/PlayableCharacter.kt
+++ src/main/java/demo/medieval_game/data/gameobject/PlayableCharacter.kt
@@ -85,9 +85,9 @@ class PlayableCharacter(
     private fun getAnimatedObjectComponent(
         renderProjection: Matrix4f
     ): AnimatedModel2D {
-        val frameSizeX = 0.111f
+        val frameSizeX = 0.066f
         val frameSizeY = 0.25f
-        val texturePathFirst = this.javaClass.getResource("/textures/KnightAtlas_ver5.png")!!.path
+        val texturePathFirst = this.javaClass.getResource("/textures/KnightAtlas_ver8.png")!!.path
 
         val textureArray = Texture2D.createInstance(
             texturePathFirst
