4be84fcc9ae84ebdaa3066f8702ac5dc9aac3556
Steyrix <steyrixio@gmail.com>
2024-01-18 00:46:49 +0400

    Add blank spaces

--- src/main/java/engine/core/controllable/Controllable.kt
+++ src/main/java/engine/core/controllable/Controllable.kt
@@ -3,5 +3,6 @@ package engine.core.controllable
 import engine.core.window.Window
 
 interface Controllable {
+
     fun input(window: Window)
 }
--- src/main/java/engine/core/entity/Entity.kt
+++ src/main/java/engine/core/entity/Entity.kt
@@ -3,6 +3,7 @@ package engine.core.entity
 import engine.feature.interaction.Interaction
 
 interface Entity {
+
     fun onAdd() {}
 
     fun consumeInteraction(interaction: Interaction) {}
