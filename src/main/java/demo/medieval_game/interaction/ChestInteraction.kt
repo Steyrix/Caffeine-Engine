b923283b2acb451b6a9d35d34089d516611769b4
Steyrix <steyrixio@gmail.com>
2024-01-18 01:26:49 +0400

    Rename

--- src/main/java/demo/medieval_game/interaction/ChestInteraction.kt
+++ src/main/java/demo/medieval_game/interaction/ChestInteraction.kt
@@ -3,6 +3,6 @@ package demo.medieval_game.interaction
 import engine.feature.interaction.Interaction
 
 sealed class ChestInteraction : Interaction {
-    object OpenChestInteraction : ChestInteraction()
-    object CloseChestInteraction : ChestInteraction()
+    object Open : ChestInteraction()
+    object Close : ChestInteraction()
 }
