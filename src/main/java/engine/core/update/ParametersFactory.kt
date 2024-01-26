c0c3656edf384bdfdf2e22b17ae06ec99e332cc9
Steyrix <steyrixio@gmail.com>
2024-01-26 14:18:49 +0400

    Implement ParametersFactory.kt

---
+++ src/main/java/engine/core/update/ParametersFactory.kt
@@ -0,0 +1,8 @@
+package engine.core.update
+
+object ParametersFactory {
+
+    fun createEmptyStatic(): SetOfStatic2DParameters {
+        return SetOfStatic2DParameters(0f, 0f, 0f, 0f, 0f)
+    }
+}
