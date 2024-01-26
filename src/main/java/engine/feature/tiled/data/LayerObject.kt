3dc718288fc008f5f9dc253587e84119ef2092f5
Steyrix <steyrixio@gmail.com>
2024-01-26 15:11:42 +0400

    Start implementing LayerObject.kt

--- src/main/java/engine/feature/tiled/data/LayerObject.kt
+++ src/main/java/engine/feature/tiled/data/LayerObject.kt
@@ -1,7 +1,17 @@
 package engine.feature.tiled.data
 
 import engine.core.entity.CompositeEntity
+import engine.core.render.Model
+import engine.core.texture.Texture2D
 
-class LayerObject : CompositeEntity() {
+class LayerObject(
+    val tileIndices: List<Int>,
+    texture2D: Texture2D,
+    uv: FloatArray
+) : CompositeEntity() {
+
+    val graphicalComponent = Model(texture2D, uv)
+
+    var isTransparent = false
 
 }
