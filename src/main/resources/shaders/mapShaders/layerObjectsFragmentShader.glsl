3c88e141f2a66743b919021c7c398737c86364d8
Steyrix <steyrixio@gmail.com>
2024-01-29 23:02:40 +0400

    Implement shader

--- src/main/resources/shaders/mapShaders/layerObjectsFragmentShader.glsl
+++ src/main/resources/shaders/mapShaders/layerObjectsFragmentShader.glsl
@@ -1,5 +1,12 @@
 #version 330
 
-void main() {
+in vec2 fragmentUV;
+out vec4 fColor;
 
+uniform sampler2D textureSample;
+uniform float transparency;
+
+void main() {
+    fColor = texture(textureSample, fragmentUV).rgba;
+    fColor.a = transparency;
 }
