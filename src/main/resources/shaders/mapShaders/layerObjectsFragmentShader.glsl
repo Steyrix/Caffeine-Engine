d0bd3656a9cf57a8ded163e9797d5b7ade5dc142
Steyrix <steyrixio@gmail.com>
2024-01-31 14:13:47 +0400

    Use uniform

--- src/main/resources/shaders/mapShaders/layerObjectsFragmentShader.glsl
+++ src/main/resources/shaders/mapShaders/layerObjectsFragmentShader.glsl
@@ -4,8 +4,14 @@ in vec2 fragmentUV;
 out vec4 fColor;
 
 uniform sampler2D textureSample;
+uniform float transparency;
 
 void main(void)
 {
     fColor = texture(textureSample, fragmentUV).rgba;
+    fColor.a = transparency;
+
+    if (fColor.a <= 0) {
+        discard;
+    }
 }
