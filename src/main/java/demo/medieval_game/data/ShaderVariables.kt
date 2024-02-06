739baf9f56868efd89469bae10534024e7eb236f
Steyrix <steyrixio@gmail.com>
2024-02-06 14:27:48 +0400

    Adjust lighting shader parameters

--- src/main/java/demo/medieval_game/data/ShaderVariables.kt
+++ src/main/java/demo/medieval_game/data/ShaderVariables.kt
@@ -1,10 +1,10 @@
 package demo.medieval_game.data
 
 /* Lighting shaders variables */
-var lightBlinkingTimeLimit = 0.1f
+var lightBlinkingTimeLimit = 0.05f
 
-var lightIntensityCap = 2f
+var lightIntensityCap = 1.5f
 
-val lightIntensityCaps = listOf(2f, 1.95f, 1.9f)
+val lightIntensityCaps = listOf(1.1f, 1.0f, 1.11f, 1.12f, 1.15f, 1.16f, 1.2f)
 
 var current = 0
