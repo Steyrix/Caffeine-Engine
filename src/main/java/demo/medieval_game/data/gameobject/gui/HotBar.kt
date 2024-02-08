91d8b46bdb1999f3006d01178482bf9ae90f78c9
Steyrix <steyrixio@gmail.com>
2024-02-08 18:59:21 +0400

    Implement HotBar.kt

---
+++ src/main/java/demo/medieval_game/data/gameobject/gui/HotBar.kt
@@ -0,0 +1,28 @@
+package demo.medieval_game.data.gameobject.gui
+
+import demo.medieval_game.ShaderController
+import demo.medieval_game.data.gameobject.hp.HealthBar
+import engine.core.entity.CompositeEntity
+import engine.core.render.Model
+import engine.core.texture.Texture2D
+import org.joml.Matrix4f
+
+class HotBar : CompositeEntity() {
+
+    private var hpBar: HealthBar? = null
+    private var manaBar: HealthBar? = null
+
+    private var skills: List<Any> = emptyList()
+
+    private var graphicalComponent: Model? = null
+
+    fun init(renderProjection: Matrix4f) {
+        val texturePath = this.javaClass.getResource("/textures/HotBarContainer.png")!!.path
+
+        graphicalComponent = Model(
+            texture = Texture2D.createInstance(texturePath)
+        ).apply {
+            shader = ShaderController.createTexturedShader(renderProjection)
+        }
+    }
+}
