e21209a6f1293598a7e207fb2b30e912ac46531e
Steyrix <steyrixio@gmail.com>
2024-02-10 17:35:45 +0400

    Add render projection to constructor args

--- src/main/java/demo/medieval_game/data/gameobject/gui/HotBarSmallCell.kt
+++ src/main/java/demo/medieval_game/data/gameobject/gui/HotBarSmallCell.kt
@@ -1,13 +1,16 @@
 package demo.medieval_game.data.gameobject.gui
 
+import demo.medieval_game.ShaderController
 import engine.core.entity.CompositeEntity
 import engine.core.entity.Entity
 import engine.core.render.Model
 import engine.core.texture.Texture2D
 import engine.core.update.SetOfParameters
 import engine.core.update.SetOfStatic2DParameters
+import org.joml.Matrix4f
 
 class HotBarSmallCell(
+    val renderProjection: Matrix4f,
     val parameters: SetOfStatic2DParameters,
     private val containerParameters: SetOfStatic2DParameters,
     private val content: Pair<Entity, SetOfParameters>? = null,
@@ -19,7 +22,9 @@ class HotBarSmallCell(
     init {
         graphicalComponent = Model(
             texture = Texture2D.createInstance(texturePath)
-        )
+        ).apply {
+            shader = ShaderController.createTexturedShader(renderProjection)
+        }
         addComponent(graphicalComponent, parameters)
 
         content?.let {
