a77a79c5842f375696670d05145e8a40a2f87b1f
Steyrix <steyrixio@gmail.com>
2024-02-10 14:25:52 +0400

    Add mana bar to gui container

--- src/main/java/demo/medieval_game/data/gameobject/gui/GuiCreator.kt
+++ src/main/java/demo/medieval_game/data/gameobject/gui/GuiCreator.kt
@@ -1,6 +1,7 @@
 package demo.medieval_game.data.gameobject.gui
 
 import demo.medieval_game.data.gameobject.gui.bar.HealthBar
+import demo.medieval_game.data.gameobject.gui.bar.ManaBar
 import demo.medieval_game.scene.MedievalGame
 import engine.core.update.SetOfStatic2DParameters
 import engine.feature.matrix.MatrixState
@@ -17,14 +18,17 @@ object GuiCreator {
 
         val containerParams = createParametersForContainer(screenWidth, screenHeight)
         val hpBarParams = createParametersForHpBar(containerParams)
+        val manaBarParams = createParametersForManaBar(containerParams)
 
         val healthBar = createHealthBar(containerParams, hpBarParams)
+        val manaBar = createManaBar(containerParams, manaBarParams)
         val guiContainer = GuiContainer(containerParams)
 
         guiContainer.init(renderProjection)
         guiContainer.addComponent(healthBar, hpBarParams)
+        guiContainer.addComponent(manaBar, manaBarParams)
         matrixState.nonTranslatedParams.addAll(
-            listOf(containerParams, hpBarParams)
+            listOf(containerParams, hpBarParams, manaBarParams)
         )
 
         return guiContainer
@@ -60,6 +64,28 @@ object GuiCreator {
         onFilledChange = {},
         projection = MedievalGame.renderProjection,
         texturePath = this.javaClass.getResource("/textures/gui/HealthBarAtlas.png")!!.path,
+        isBoundToParams = false
+    )
+
+    private fun createParametersForManaBar(
+        containerParams: SetOfStatic2DParameters
+    ) = SetOfStatic2DParameters(
+        x = containerParams.x + containerParams.xSize * 0.509f,
+        y = containerParams.y + containerParams.ySize * 0.234f,
+        xSize = containerParams.xSize * 0.202f,
+        ySize = containerParams.ySize * 0.205f,
+        rotationAngle = 0f
+    )
+
+    private fun createManaBar(
+        containerParams: SetOfStatic2DParameters,
+        manaBarParams: SetOfStatic2DParameters
+    ) = ManaBar(
+        objParams = containerParams,
+        barParams = manaBarParams,
+        onFilledChange = {},
+        projection = MedievalGame.renderProjection,
+        texturePath = this.javaClass.getResource("/textures/gui/ManaBarAtlas.png")!!.path,
         isBoundToParams = false
     )
 }
