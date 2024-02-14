06df31af2967b9a024d8ef95502b82caa09d2795
Steyrix <steyrixio@gmail.com>
2024-02-14 16:19:49 +0400

    Add logic for tracking currently interacting entities

--- src/main/java/engine/feature/interaction/BoxInteractionContext.kt
+++ src/main/java/engine/feature/interaction/BoxInteractionContext.kt
@@ -11,6 +11,8 @@ class BoxInteractionContext : InteractionContext<BoundingBox> {
 
     override val isInteracting = { target: Entity, agent: Entity -> isIntersecting(target, agent) }
 
+    override val currentlyInteractingAgents: MutableMap<Entity, MutableSet<Entity>> = mutableMapOf()
+
     private fun isIntersecting(target: Entity, agent: Entity): Boolean {
         if (target == agent) return false
 
