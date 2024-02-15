4896140c74d07fd036adf93c1df0078c18c0acc0
Steyrix <steyrixio@gmail.com>
2024-02-15 15:30:51 +0400

    Inherit from InteractionEvent

--- src/main/java/demo/medieval_game/interaction/event/MedievalGameInteractionEvent.kt
+++ src/main/java/demo/medieval_game/interaction/event/MedievalGameInteractionEvent.kt
@@ -1,8 +1,9 @@
 package demo.medieval_game.interaction.event
 
 import engine.core.entity.Entity
+import engine.feature.interaction.broadcast.InteractionEvent
 
-sealed interface MedievalGameInteractionEvent
+sealed interface MedievalGameInteractionEvent : InteractionEvent
 
 data class Loot(
     val content: MutableList<Entity>
