9728013777c258a90acd6c4fc2145b53e9d7808d
Steyrix <steyrixio@gmail.com>
2024-02-16 15:36:15 +0400

    Add initialising

--- src/main/java/demo/medieval_game/interaction/event_receivers/GuiController.kt
+++ src/main/java/demo/medieval_game/interaction/event_receivers/GuiController.kt
@@ -2,6 +2,7 @@ package demo.medieval_game.interaction.event_receivers
 
 import demo.medieval_game.data.gameobject.gui.chest.ChestGuiContainer
 import demo.medieval_game.interaction.event.Loot
+import demo.medieval_game.scene.MedievalGame
 import engine.core.entity.CompositeEntity
 import engine.core.geometry.Point2D
 import engine.core.update.ParametersFactory
@@ -10,9 +11,15 @@ import engine.feature.interaction.broadcast.InteractionEvent
 
 object GuiController : CompositeEntity(), EventReceiver {
 
+    private val renderProjection = MedievalGame.renderProjection
+
     private val chestGui: ChestGuiContainer = ChestGuiContainer(ParametersFactory.createEmptyStatic())
 
     var isInputBlocked: Boolean = false
+
+    init {
+        chestGui.init(renderProjection)
+    }
 
     private fun showChestGui(pos: Point2D) {
         chestGui.parameters.apply {
