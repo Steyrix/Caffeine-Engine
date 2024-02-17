9a8091ae458d0f2a0b2081c894c0cf9abfc6f47f
Steyrix <steyrixio@gmail.com>
2024-02-17 15:46:23 +0400

    Use plain entity and copy params

--- src/main/java/demo/medieval_game/data/gameobject/inventory_item/LootCreator.kt
+++ src/main/java/demo/medieval_game/data/gameobject/inventory_item/LootCreator.kt
@@ -2,7 +2,6 @@ package demo.medieval_game.data.gameobject.inventory_item
 
 import demo.medieval_game.ShaderController
 import demo.medieval_game.scene.MedievalGame
-import engine.core.entity.CompositeEntity
 import engine.core.render.Model
 import engine.core.texture.Texture2D
 import engine.core.update.SetOfStatic2DParameters
@@ -10,24 +9,20 @@ import engine.core.update.SetOfStatic2DParameters
 object LootCreator {
 
     fun createBasicLoot(parameters: SetOfStatic2DParameters): MutableList<InventoryItemWrapper> {
+        val params = parameters.copy()
         val out = mutableListOf<InventoryItemWrapper>()
 
-        val sword = CompositeEntity().apply {
+        val path = this.javaClass.getResource("/textures/gui/item/weapon/WarriorSwordIcon.png")!!.path
 
-            val path = this.javaClass.getResource("/textures/gui/item/weapon/WarriorSwordIcon.png")!!.path
-
-            addComponent(
-                Model(
-                    texture = Texture2D.createInstance(path)
-                ).also {
-                       it.shader = ShaderController.createTexturedShader(MedievalGame.renderProjection)
-                },
-                parameters
-            )
+        val sword = Model(
+            texture = Texture2D.createInstance(path)
+        ).also {
+            it.shader = ShaderController.createTexturedShader(MedievalGame.renderProjection)
+            it.zLevel = 1f
         }
 
         val wrapper = InventoryItemWrapper(
-            parameters,
+            params,
             ItemRarity.COMMON,
             sword
         ).also {
