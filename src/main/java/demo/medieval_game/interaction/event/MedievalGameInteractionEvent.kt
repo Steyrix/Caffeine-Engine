93b45d17cae81bfb0d933f914ed0474f362b7b26
Steyrix <steyrixio@gmail.com>
2024-02-17 14:47:28 +0400

    Set content on gui creation

--- src/main/java/demo/medieval_game/interaction/event/MedievalGameInteractionEvent.kt
+++ src/main/java/demo/medieval_game/interaction/event/MedievalGameInteractionEvent.kt
@@ -1,13 +1,13 @@
 package demo.medieval_game.interaction.event
 
-import engine.core.entity.Entity
+import demo.medieval_game.data.gameobject.inventory_item.InventoryItemWrapper
 import engine.core.geometry.Point2D
 import engine.feature.interaction.broadcast.InteractionEvent
 
 sealed interface MedievalGameInteractionEvent : InteractionEvent
 
 data class OpenChest(
-    val content: MutableList<Entity>,
+    val content: MutableList<InventoryItemWrapper>,
     val pos: Point2D
 ) : MedievalGameInteractionEvent
 
