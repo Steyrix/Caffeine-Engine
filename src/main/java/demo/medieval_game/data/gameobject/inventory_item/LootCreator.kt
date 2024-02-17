cba418df353e75add0d300c7411c1e9822043ca6
Steyrix <steyrixio@gmail.com>
2024-02-17 14:55:19 +0400

    Init with projection

--- src/main/java/demo/medieval_game/data/gameobject/inventory_item/LootCreator.kt
+++ src/main/java/demo/medieval_game/data/gameobject/inventory_item/LootCreator.kt
@@ -1,5 +1,7 @@
 package demo.medieval_game.data.gameobject.inventory_item
 
+import demo.medieval_game.ShaderController
+import demo.medieval_game.scene.MedievalGame
 import engine.core.entity.CompositeEntity
 import engine.core.render.Model
 import engine.core.texture.Texture2D
@@ -7,14 +9,19 @@ import engine.core.update.SetOfStatic2DParameters
 
 object LootCreator {
 
-    fun createBasicLoot(parameters: SetOfStatic2DParameters): MutableList<CompositeEntity> {
-        val out = mutableListOf<CompositeEntity>()
+    fun createBasicLoot(parameters: SetOfStatic2DParameters): MutableList<InventoryItemWrapper> {
+        val out = mutableListOf<InventoryItemWrapper>()
 
         val sword = CompositeEntity().apply {
+
+            val path = this.javaClass.getResource("/textures/gui/item/weapon/WarriorSwordIcon.png")!!.path
+
             addComponent(
                 Model(
-                    texture = Texture2D.createInstance("/textures/gui/item/weapon/WarriorSwordIcon.png")
-                ),
+                    texture = Texture2D.createInstance(path)
+                ).also {
+                       it.shader = ShaderController.createTexturedShader(MedievalGame.renderProjection)
+                },
                 parameters
             )
         }
@@ -23,7 +30,9 @@ object LootCreator {
             parameters,
             ItemRarity.COMMON,
             sword
-        )
+        ).also {
+            it.init(MedievalGame.renderProjection)
+        }
 
         out.add(wrapper)
 
