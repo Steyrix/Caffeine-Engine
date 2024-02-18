c54ab3e2a8180af4a7c55cf61b404814be2a135d
Steyrix <steyrixio@gmail.com>
2024-02-18 13:03:28 +0400

    Build up components

--- src/main/java/demo/medieval_game/data/gameobject/gui/button/GenericButton.kt
+++ src/main/java/demo/medieval_game/data/gameobject/gui/button/GenericButton.kt
@@ -5,10 +5,12 @@ import demo.medieval_game.scene.MedievalGame
 import engine.core.entity.CompositeEntity
 import engine.core.render.Model
 import engine.core.texture.Texture2D
+import engine.core.update.SetOfStatic2DParameters
 
 class GenericButton(
     onClick: (Any?) -> Unit,
-    texturePath: String
+    texturePath: String,
+    private val parameters: SetOfStatic2DParameters
 ) : CompositeEntity() {
 
     companion object {
@@ -19,6 +21,11 @@ class GenericButton(
 
     private var model: Model? = null
 
+    private val controller = ButtonController { arg ->
+        onClick.invoke(arg)
+        isPressed = !isPressed
+    }
+
     init {
         val texture = Texture2D.createInstance(texturePath)
 
@@ -27,6 +34,9 @@ class GenericButton(
             zLevel = 1f
             isPartOfWorldTranslation = false
         }
+
+        addComponent(model!!, parameters)
+        addComponent(controller, parameters)
     }
 
     override fun update(deltaTime: Float) {
