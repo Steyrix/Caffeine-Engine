3f07a5213322f91f01885dc5e6cad31a2b654b53
Steyrix <steyrixio@gmail.com>
2024-02-19 14:53:51 +0400

    Add separate open and close interaction objects

--- src/main/java/demo/medieval_game/interaction/ChestInteraction.kt
+++ src/main/java/demo/medieval_game/interaction/ChestInteraction.kt
@@ -5,5 +5,9 @@ import engine.feature.interaction.Interaction
 sealed class ChestInteraction : Interaction {
     object OpenClose : ChestInteraction()
 
+    object Open : ChestInteraction()
+
+    object Close : ChestInteraction()
+
     object Highlight : ChestInteraction()
 }
