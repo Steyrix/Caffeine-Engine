a5717feac983ed55c90173d4b78345d78793e628
Steyrix <steyrixio@gmail.com>
2024-02-23 16:34:55 +0400

    Create animations for sword atlas

--- src/main/java/demo/medieval_game/data/gameobject/character/CharacterAnimations.kt
+++ src/main/java/demo/medieval_game/data/gameobject/character/CharacterAnimations.kt
@@ -17,7 +17,94 @@ object CharacterAnimations {
     const val STRIKE_L = "STRIKE_L"
     const val STRIKE_U = "STRIKE_U"
     const val STRIKE_D = "STRIKE_D"
+    const val ALT_STRIKE_R = "ALT_STRIKE_R"
+    const val ALT_STRIKE_L = "ALT_STRIKE_L"
+    const val ALT_STRIKE_U = "ALT_STRIKE_U"
+    const val ALT_STRIKE_D = "ALT_STRIKE_D"
 }
+
+val characterWithSwordAnimation = mutableListOf<Animation>(
+    SequenceAtlasAnimation(
+        name = CharacterAnimations.IDLE_R,
+        frames = FrameParametersGenerator.generate(Pair(0, 0), Pair(4, 4), 0.1111f, 0.0714f),
+        timeLimit = 1f
+    ),
+    SequenceAtlasAnimation(
+        name = CharacterAnimations.IDLE_L,
+        frames = FrameParametersGenerator.generate(Pair(0, 0), Pair(0, 0), 0.1111f, 0.0714f),
+        timeLimit = 1f
+    ),
+    SequenceAtlasAnimation(
+        name = CharacterAnimations.IDLE_U,
+        frames = FrameParametersGenerator.generate(Pair(0, 0), Pair(11, 11), 0.1111f, 0.0714f),
+        timeLimit = 1f
+    ),
+    SequenceAtlasAnimation(
+        name = CharacterAnimations.IDLE_D,
+        frames = FrameParametersGenerator.generate(Pair(0, 0), Pair(8, 8), 0.1111f, 0.0714f),
+        timeLimit = 1f
+    ),
+    SequenceAtlasAnimation(
+        name = CharacterAnimations.WALK_R,
+        frames = FrameParametersGenerator.generate(Pair(1, 8), Pair(4, 4), 0.1111f, 0.0714f),
+        timeLimit = 0.05f
+    ),
+    SequenceAtlasAnimation(
+        name = CharacterAnimations.WALK_L,
+        frames = FrameParametersGenerator.generate(Pair(8, 1), Pair(0, 0), 0.1111f, 0.0714f),
+        timeLimit = 0.05f
+    ),
+    SequenceAtlasAnimation(
+        name = CharacterAnimations.WALK_U,
+        frames = FrameParametersGenerator.generate(Pair(1, 8), Pair(11, 11), 0.1111f, 0.0714f),
+        timeLimit = 0.05f
+    ),
+    SequenceAtlasAnimation(
+        name = CharacterAnimations.WALK_D,
+        frames = FrameParametersGenerator.generate(Pair(1, 8), Pair(8, 8), 0.1111f, 0.0714f),
+        timeLimit = 0.05f
+    ),
+    SequenceAtlasAnimation(
+        name = CharacterAnimations.STRIKE_U,
+        frames = FrameParametersGenerator.generate(Pair(0, 5), Pair(12, 12), 0.1111f, 0.0714f),
+        timeLimit = 0.1f
+    ),
+    SequenceAtlasAnimation(
+        name = CharacterAnimations.ALT_STRIKE_U,
+        frames = FrameParametersGenerator.generate(Pair(0, 5), Pair(13, 13), 0.1111f, 0.0714f),
+        timeLimit = 0.1f
+    ),
+    SequenceAtlasAnimation(
+        name = CharacterAnimations.STRIKE_L,
+        frames = FrameParametersGenerator.generate(Pair(0, 5), Pair(1, 1), 0.1111f, 0.0714f),
+        timeLimit = 0.1f
+    ),
+    SequenceAtlasAnimation(
+        name = CharacterAnimations.ALT_STRIKE_L,
+        frames = FrameParametersGenerator.generate(Pair(0, 5), Pair(2, 2), 0.1111f, 0.0714f),
+        timeLimit = 0.1f
+    ),
+    SequenceAtlasAnimation(
+        name = CharacterAnimations.STRIKE_D,
+        frames = FrameParametersGenerator.generate(Pair(0, 5), Pair(9, 9), 0.1111f, 0.0714f),
+        timeLimit = 0.1f
+    ),
+    SequenceAtlasAnimation(
+        name = CharacterAnimations.ALT_STRIKE_D,
+        frames = FrameParametersGenerator.generate(Pair(0, 5), Pair(10, 10), 0.1111f, 0.0714f),
+        timeLimit = 0.1f
+    ),
+    SequenceAtlasAnimation(
+        name = CharacterAnimations.STRIKE_R,
+        frames = FrameParametersGenerator.generate(Pair(0, 5), Pair(5, 5), 0.1111f, 0.0714f),
+        timeLimit = 0.1f
+    ),
+    SequenceAtlasAnimation(
+        name = CharacterAnimations.ALT_STRIKE_R,
+        frames = FrameParametersGenerator.generate(Pair(0, 5), Pair(6, 6), 0.1111f, 0.0714f),
+        timeLimit = 0.1f
+    ),
+)
 
 val characterAnimations = mutableListOf<Animation>(
     SequenceAtlasAnimation(
