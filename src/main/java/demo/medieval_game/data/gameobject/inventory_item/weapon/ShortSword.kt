83478fe6ae6562c656f310b43309f5c6de3e0fcf
Steyrix <steyrixio@gmail.com>
2024-02-23 15:51:04 +0400

    Create short sword abstract class

---
+++ src/main/java/demo/medieval_game/data/gameobject/inventory_item/weapon/ShortSword.kt
@@ -0,0 +1,24 @@
+package demo.medieval_game.data.gameobject.inventory_item.weapon
+
+
+import demo.medieval_game.data.gameobject.inventory_item.ItemRarity
+import engine.core.render.Model
+import engine.core.texture.Texture2D
+import engine.feature.animation.Animation
+
+abstract class ShortSword(
+    texturePath: String,
+    override val rarity: ItemRarity,
+    override val afterEquipAnimation: List<Animation>,
+) : Weapon() {
+
+    override val onUseAnimations = emptyList<Animation>()
+
+    final override val drawableComponent: Model
+
+    init {
+        drawableComponent = Model(
+            texture = Texture2D.createInstance(texturePath)
+        )
+    }
+}
