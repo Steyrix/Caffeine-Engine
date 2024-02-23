202fc7e0c306d2acd32c5a53859cc6d50964f2e9
Steyrix <steyrixio@gmail.com>
2024-02-23 16:01:33 +0400

    Add parameters property to constructor

--- src/main/java/demo/medieval_game/data/gameobject/inventory_item/weapon/ShortSword.kt
+++ src/main/java/demo/medieval_game/data/gameobject/inventory_item/weapon/ShortSword.kt
@@ -6,10 +6,12 @@ import demo.medieval_game.data.gameobject.inventory_item.ItemRarity
 import demo.medieval_game.scene.MedievalGame
 import engine.core.render.Model
 import engine.core.texture.Texture2D
+import engine.core.update.SetOfStatic2DParameters
 import engine.feature.animation.Animation
 
 abstract class ShortSword(
     texturePath: String,
+    private val parameters: SetOfStatic2DParameters,
     override val baseDamage: Float,
     override val rarity: ItemRarity,
     override val afterEquipAnimation: List<Animation>,
@@ -27,5 +29,7 @@ abstract class ShortSword(
             zLevel = 1f
             isPartOfWorldTranslation = false
         }
+
+        addComponent(drawableComponent, parameters)
     }
 }
