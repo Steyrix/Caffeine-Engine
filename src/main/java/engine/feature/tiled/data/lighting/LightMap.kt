97cd9a9265b2c2e7f872f712d10ec9ab2187e22b
Steyrix <steyrixio@gmail.com>
2024-03-28 16:50:52 +0400

    Start implementing light intensity calculation

--- src/main/java/engine/feature/tiled/data/lighting/LightMap.kt
+++ src/main/java/engine/feature/tiled/data/lighting/LightMap.kt
@@ -1,11 +1,34 @@
 package engine.feature.tiled.data.lighting
 
+import engine.core.geometry.Point2D
 import engine.core.update.SetOfStatic2DParameters
-import engine.feature.tiled.data.layer.Layer
+import engine.feature.tiled.data.TileMap
 
 class LightMap(
-    layer: Layer,
     lightSources: List<SetOfStatic2DParameters>
 ) {
+
+    companion object {
+        fun generateInstance(
+            tileMap: TileMap,
+            lightSources: List<SetOfStatic2DParameters>
+        ) {
+            val outLightIntensityList = mutableListOf<Float>()
+
+            val tilesCount = tileMap.tilesCount
+            val tilePositions = mutableListOf<Point2D>()
+
+            for (index in 0 until tilesCount) {
+                tilePositions.add(
+                    tileMap.getTilePosition(index)
+                )
+            }
+
+            tilePositions.forEach {
+
+            }
+        }
+    }
+
 
 }
