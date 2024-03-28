3eba690533a3d0d49776a321a4bfee61dcdfdaad
Steyrix <steyrixio@gmail.com>
2024-03-28 16:31:20 +0400

    Introduce LightMap class

---
+++ src/main/java/engine/feature/tiled/data/lighting/LightMap.kt
@@ -0,0 +1,11 @@
+package engine.feature.tiled.data.lighting
+
+import engine.core.update.SetOfStatic2DParameters
+import engine.feature.tiled.data.layer.Layer
+
+class LightMap(
+    layer: Layer,
+    lightSources: List<SetOfStatic2DParameters>
+) {
+
+}
