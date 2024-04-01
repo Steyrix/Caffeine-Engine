8b356f94110e0232debfbbb772fca866039542a6
Steyrix <steyrixio@gmail.com>
2024-04-01 14:09:54 +0400

    Delete unused imports

--- src/main/java/engine/feature/tiled/data/lighting/LightMap.kt
+++ src/main/java/engine/feature/tiled/data/lighting/LightMap.kt
@@ -1,17 +1,11 @@
 package engine.feature.tiled.data.lighting
 
 import engine.core.entity.CompositeEntity
-import engine.core.geometry.Point2D
 import engine.core.render.Model
 import engine.core.shader.Shader
 import engine.core.update.SetOfParameters
 import engine.core.update.SetOfStatic2DParameters
 import engine.feature.tiled.data.TileMap
-import org.joml.Vector2f
-import org.joml.Vector3f
-import org.lwjgl.opengl.GL11.*
-import kotlin.math.abs
-import kotlin.math.min
 
 class LightMap(
     val parameters: SetOfParameters,
