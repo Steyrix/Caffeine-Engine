11d07885403509f0361acdfd9a2fadc4c459db84
Steyrix <steyrixio@gmail.com>
2024-04-03 19:05:35 +0400

    Encapsulate lightMap creation

--- src/main/java/engine/feature/tiled/scene/TileMapScene.kt
+++ src/main/java/engine/feature/tiled/scene/TileMapScene.kt
@@ -6,12 +6,19 @@ import engine.core.scene.GameContext
 import engine.core.scene.Scene
 import engine.core.scene.SceneIntent
 import engine.core.session.Session
+import engine.core.update.SetOfStatic2DParameters
 import engine.feature.collision.tiled.TiledCollisionContext
+import engine.feature.tiled.data.lighting.LightMap
+import engine.feature.tiled.data.lighting.LightSource
 import org.joml.Matrix4f
 
 abstract class TileMapScene(
     projection: Matrix4f
 ) : Scene {
+
+    companion object {
+        private const val LIGHTMAP_PRECISION = 50f
+    }
 
     override val renderProjection = projection
 
@@ -19,6 +26,10 @@ abstract class TileMapScene(
 
     protected var tiledCollisionContext: TiledCollisionContext? = null
     protected var tiledMap: TileMapEntity? = null
+
+    private val lightSources: MutableList<LightSource> = mutableListOf()
+
+    protected var lightMap: LightMap? = null
 
     override fun init(session: Session, intent: SceneIntent?) {
         super.init(session, intent)
@@ -41,6 +52,38 @@ abstract class TileMapScene(
             )
             it.addToCollisionContext(tiledCollisionContext!!)
         }
+
+        if (lightSources.isNotEmpty()) {
+            lightMap = generateLightMap(renderProjection, lightSources)
+        }
+
+    }
+
+    private fun generateLightMap(
+        renderProjection: Matrix4f,
+        lightSources: MutableList<LightSource>
+    ): LightMap {
+        tiledMap?.mapComponent?.let {
+
+            val litLightSources = mutableListOf<LightSource>()
+            litLightSources.addAll(lightSources.filter { it.isLit })
+
+            return LightMap(
+                precision = LIGHTMAP_PRECISION,
+                projection = renderProjection,
+                parameters = SetOfStatic2DParameters(
+                    x = 0f,
+                    y = 0f,
+                    xSize = screenWidth * (screenWidth / LIGHTMAP_PRECISION),
+                    ySize = screenHeight * (screenHeight / LIGHTMAP_PRECISION),
+                    rotationAngle = 0f
+                ),
+                tileMap = it,
+                lightSources = litLightSources,
+                screenSizeX = screenWidth,
+                screenSizeY = screenHeight
+            )
+        } ?: throw IllegalStateException()
     }
 
     abstract fun initTileMap(
@@ -49,7 +92,26 @@ abstract class TileMapScene(
         screenHeight: Float
     ): TileMapEntity
 
-    fun spawn(entity: GameEntity, spawnOptions: SpawnOptions) {
+    fun spawn(entity: GameEntity, spawnOptions: SpawnOptions) {}
 
+    fun addLightSource(lightSource: LightSource) {
+        lightSources.add(lightSource)
+        if (lightSource.isLit) {
+            lightMap = generateLightMap(renderProjection, lightSources)
+        }
+    }
+
+    fun removeLightSource(lightSource: LightSource) {
+        lightSources.remove(lightSource)
+        if (lightSource.isLit) {
+            lightMap = generateLightMap(renderProjection, lightSources)
+        }
+    }
+
+    fun setLightSourceLit(lightSource: LightSource, value: Boolean) {
+        if (value != lightSource.isLit) {
+            lightSource.isLit = value
+            lightMap = generateLightMap(renderProjection, lightSources)
+        }
     }
 }
