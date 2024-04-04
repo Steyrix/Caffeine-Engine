a67b742b81181734180d0a628c301fada747b9b9
Steyrix <steyrixio@gmail.com>
2024-04-04 14:40:14 +0400

    Add extension for Point2D

--- src/main/java/engine/core/parameters/ParametersExt.kt
+++ src/main/java/engine/core/parameters/ParametersExt.kt
@@ -1,6 +1,8 @@
 package engine.core.parameters
 
+import engine.core.geometry.Point2D
 import engine.core.update.SetOf2DParametersWithVelocity
+import engine.core.update.SetOfParameters
 import engine.core.update.SetOfStatic2DParameters
 
 fun SetOf2DParametersWithVelocity.toStatic(): SetOfStatic2DParameters {
@@ -11,4 +13,8 @@ fun SetOf2DParametersWithVelocity.toStatic(): SetOfStatic2DParameters {
         ySize,
         rotationAngle
     )
+}
+
+fun Point2D.isInBounds(params: SetOfParameters): Boolean {
+    return (this.x <= params.xSize && x >= params.x) && (this.y <= params.ySize && y >= params.y)
 }
