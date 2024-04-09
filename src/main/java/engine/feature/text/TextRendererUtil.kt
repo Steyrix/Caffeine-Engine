49cc89c571205ab47c9d8dc12f635064570161b9
Steyrix <steyrixio@gmail.com>
2024-04-09 13:16:04 +0400

    Remove unused import

--- src/main/java/engine/feature/text/TextRendererUtil.kt
+++ src/main/java/engine/feature/text/TextRendererUtil.kt
@@ -3,10 +3,8 @@ package engine.feature.text
 import engine.core.texture.Texture2D
 import engine.core.geometry.Point2D
 import java.awt.Dimension
-import java.util.InvalidPropertiesFormatException
 
 internal object TextRendererUtil {
-    private const val ATLAS_WRONG_FORMAT_ERROR_TEXT = "Invalid texture atlas format"
 
     fun generateMap(
         charSize: Dimension,
@@ -14,25 +12,11 @@ internal object TextRendererUtil {
         characters: List<Char>
     ): HashMap<Char, Point2D> {
         val out = HashMap<Char, Point2D>()
-        val xStep: Int
-        val yStep: Int
         val charsCount: Int
 
-        val widthIsValid = textureAtlas.getWidthF() % charSize.getWidth() == 0.0
-        val heightIsValid = textureAtlas.getHeightF() % charSize.getHeight() == 0.0
-
-        if (widthIsValid && heightIsValid) {
-            xStep = (textureAtlas.getWidthF() / charSize.getWidth()).toInt()
-            yStep = (textureAtlas.getHeightF() / charSize.getHeight()).toInt()
-            charsCount = xStep * yStep
-            if (charsCount != characters.size)
-                throw InvalidPropertiesFormatException(
-                    "$ATLAS_WRONG_FORMAT_ERROR_TEXT: " +
-                            "\n Calculated number of characters = $charsCount" +
-                            "\n Actual number of characters = ${characters.size}"
-                )
-        } else
-            throw InvalidPropertiesFormatException(ATLAS_WRONG_FORMAT_ERROR_TEXT)
+        val xStep: Int = (textureAtlas.getWidthF() / charSize.getWidth()).toInt()
+        val yStep: Int = (textureAtlas.getHeightF() / charSize.getHeight()).toInt()
+        charsCount = xStep * yStep
 
         var j = 0
         var k = 0
