afc07a8cd8fdede990f7c67a1c9a4c2cae0155a8
Steyrix <steyrixio@gmail.com>
2024-04-22 15:28:40 +0400

    Implement loadTexture method

--- src/main/java/engine/core/ResourceLoader.kt
+++ src/main/java/engine/core/ResourceLoader.kt
@@ -1,8 +1,14 @@
 package engine.core
 
+import engine.core.texture.Texture2D
+
 object ResourceLoader {
 
     fun get(path: String): String {
         return this.javaClass.getResource(path)!!.path
+    }
+
+    fun loadTexture(path: String): Texture2D {
+        return Texture2D.createInstance(get(path))
     }
 }
