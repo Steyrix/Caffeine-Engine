0627ce1fcac8c99904fa61b4ed99264da4d75c4e
Steyrix <steyrixio@gmail.com>
2024-05-15 12:52:38 +0400

    Add texturePath parameter to AtlasData.kt

--- src/main/java/engine/feature/animation/AtlasData.kt
+++ src/main/java/engine/feature/animation/AtlasData.kt
@@ -3,5 +3,6 @@ package engine.feature.animation
 data class AtlasData(
     val frameWidth: Float,
     val frameHeight: Float,
-    val animations: List<Animation>
+    val animations: List<Animation>,
+    val texturePath: String? = null
 )
