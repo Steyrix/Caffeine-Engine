7193ed500e9d5b56dad320107fd093376932a02e
Steyrix <pevnev.georgy@gmail.com>
2024-06-16 01:44:54 +0300

    Create NoiseGenerator object and base method carcasses

---
+++ src/main/java/engine/feature/procedural/NoiseGenerator.kt
@@ -0,0 +1,23 @@
+package engine.feature.procedural
+
+import engine.core.geometry.Point2D
+import org.joml.Vector2f
+
+object NoiseGenerator {
+
+    fun getNoiseForCoordinate(pos: Point2D): Float {
+        TODO()
+    }
+
+    private fun interpolate(a0: Float, a1: Float, w: Float): Float {
+        TODO()
+    }
+
+    private fun randomGradient(ix: Int, iy: Int): Vector2f {
+        TODO()
+    }
+
+    private fun dotProduct(x: Vector2f, y: Vector2f): Float {
+        TODO()
+    }
+}
