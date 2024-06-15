73d307876dca7ef1793f41907f044b869ddff1df
Steyrix <pevnev.georgy@gmail.com>
2024-06-16 01:59:38 +0300

    Inline variable

--- src/main/java/engine/feature/procedural/NoiseGenerator.kt
+++ src/main/java/engine/feature/procedural/NoiseGenerator.kt
@@ -5,11 +5,9 @@ import engine.core.geometry.Point2D
 object NoiseGenerator {
 
     fun getNoiseForCoordinate(pos: Point2D): Float {
-        val result = OpenSimplex2S.noise2_ImproveX(
+        return OpenSimplex2S.noise2_ImproveX(
             System.currentTimeMillis(), pos.x.toDouble(), pos.y.toDouble()
         )
-
-        return result
     }
 
 }
