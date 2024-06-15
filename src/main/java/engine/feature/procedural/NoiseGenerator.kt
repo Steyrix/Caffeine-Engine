6e3c2ff4716a294c0b5b20b2f4774f151d135897
Steyrix <pevnev.georgy@gmail.com>
2024-06-16 01:59:22 +0300

    Use OpenSimplex for generating noise

--- src/main/java/engine/feature/procedural/NoiseGenerator.kt
+++ src/main/java/engine/feature/procedural/NoiseGenerator.kt
@@ -1,23 +1,15 @@
 package engine.feature.procedural
 
 import engine.core.geometry.Point2D
-import org.joml.Vector2f
 
 object NoiseGenerator {
 
     fun getNoiseForCoordinate(pos: Point2D): Float {
-        TODO()
+        val result = OpenSimplex2S.noise2_ImproveX(
+            System.currentTimeMillis(), pos.x.toDouble(), pos.y.toDouble()
+        )
+
+        return result
     }
 
-    private fun interpolate(a0: Float, a1: Float, w: Float): Float {
-        TODO()
-    }
-
-    private fun randomGradient(ix: Int, iy: Int): Vector2f {
-        TODO()
-    }
-
-    private fun dotProduct(x: Vector2f, y: Vector2f): Float {
-        TODO()
-    }
 }
