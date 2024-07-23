c410214b389fdd53ae4aa052a60430e75d6198c9
Steyrix <steyrixio@gmail.com>
2024-07-23 15:03:28 +0400

    Remove mainShader parameter

--- src/main/java/engine/feature/tiled/data/TileMapShaders.kt
+++ src/main/java/engine/feature/tiled/data/TileMapShaders.kt
@@ -3,8 +3,7 @@ package engine.feature.tiled.data
 import engine.core.shader.Shader
 
 data class TileMapShaders(
-    val mainShader: Shader? = null,
-    val objectShader: Shader? = null,
-    val objectShaderCreator: (() -> Shader?)? = null,
-    val debugShader: Shader? = null
+    var objectShader: Shader? = null,
+    var objectShaderCreator: (() -> Shader?)? = null,
+    var debugShader: Shader? = null
 )
