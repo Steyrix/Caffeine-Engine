a5ee302ba6cffc098070fb8b6cf88402873cb8e2
Steyrix <steyrixio@gmail.com>
2024-07-23 15:00:04 +0400

    Add default values

--- src/main/java/engine/feature/tiled/data/TileMapShaders.kt
+++ src/main/java/engine/feature/tiled/data/TileMapShaders.kt
@@ -3,8 +3,8 @@ package engine.feature.tiled.data
 import engine.core.shader.Shader
 
 data class TileMapShaders(
-    val mainShader: Shader?,
-    val objectShader: Shader?,
-    val objectShaderCreator: () -> Shader?,
-    val debugShader: Shader?
+    val mainShader: Shader? = null,
+    val objectShader: Shader? = null,
+    val objectShaderCreator: (() -> Shader?)? = null,
+    val debugShader: Shader? = null
 )
