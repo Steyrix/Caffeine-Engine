d7209d7f637a9d4b113fb5e9ad1b89f5be7a6066
Steyrix <steyrixio@gmail.com>
2024-08-03 15:15:42 +0400

    Define value in constructor

--- src/main/java/engine/feature/procedural/Noise.kt
+++ src/main/java/engine/feature/procedural/Noise.kt
@@ -2,9 +2,9 @@ package engine.feature.procedural
 
 interface NoiseParameterSet
 
-abstract class NoiseParameter<T: Comparable<T>> {
-
-    abstract var value: T
+abstract class NoiseParameter<T : Comparable<T>>(
+    var value: T
+) {
 
     // abstract fun getRange(): ClosedRange<T>
 }
