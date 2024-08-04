9b86f58922d8c125dee65323d9daba10e49f5083
Steyrix <steyrixio@gmail.com>
2024-08-04 12:21:50 +0400

    Make NoiseParameter non-generic

--- src/main/java/engine/feature/procedural/Noise.kt
+++ src/main/java/engine/feature/procedural/Noise.kt
@@ -2,9 +2,6 @@ package engine.feature.procedural
 
 interface NoiseParameterSet
 
-class NoiseParameter<T : Comparable<T>>(
-    var value: T
-) {
-
-    // abstract fun getRange(): ClosedRange<T>
-}
+class NoiseParameter (
+    var value: Float
+)
