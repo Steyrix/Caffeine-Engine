bb801c0dd1247c83378bb7a0dc95633d13f8716b
Steyrix <steyrixio@gmail.com>
2024-08-04 12:10:23 +0400

    Make ProceduralEnum.kt an abstract class, add collection for noise parameters, implement method for identifying if enum type is matching parameters

--- src/main/java/engine/feature/procedural/ProceduralEnum.kt
+++ src/main/java/engine/feature/procedural/ProceduralEnum.kt
@@ -1,8 +1,19 @@
 package engine.feature.procedural
 
-interface ProceduralEnum {
+typealias MinMax = Pair<Float, Float>
+typealias NoiseValue = NoiseParameter<Float>
 
-    val parameters: List<NoiseParameter<Float>>
+abstract class ProceduralEnum(
+    private val rangeMap: Map<NoiseValue, MinMax>
+) {
+    fun checkIfMatch(): Boolean {
+        rangeMap.entries.forEach {
+            val condition = it.key.value >= it.value.first && it.key.value <= it.value.second
+            if (!condition) {
+                return false
+            }
+        }
 
-    fun checkIfMatch(): Boolean
+        return true
+    }
 }
