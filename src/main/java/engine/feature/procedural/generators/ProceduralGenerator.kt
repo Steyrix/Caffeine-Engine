0ce744b094305a771524ba7fdce71d96a7e9b209
Steyrix <steyrixio@gmail.com>
2024-09-01 13:46:57 +0400

    Fix normalizeForTileSet method and heavily document it. Document other parts of a class and delete several todos. Add stubs

--- src/main/java/engine/feature/procedural/generators/ProceduralGenerator.kt
+++ src/main/java/engine/feature/procedural/generators/ProceduralGenerator.kt
@@ -3,13 +3,13 @@ package engine.feature.procedural.generators
 import engine.core.geometry.Point2D
 import engine.feature.procedural.MapElementType
 import engine.feature.procedural.OpenSimplex2S
-import engine.feature.tiled.data.TileMap
 import engine.feature.tiled.data.TileSet
 import engine.feature.tiled.data.layer.Layer
 import engine.feature.tiled.data.layer.TileLayer
-import kotlin.random.Random
 
-// TODO: (number in tileIdsData) to (number in tileSet)
+/**
+ * (number in tileIdsData) to (number in tileSet)
+ */
 typealias NormalizedData = MutableList<Pair<Point2D, Int>>
 
 class ProceduralGenerator(
@@ -24,7 +24,11 @@ class ProceduralGenerator(
 
     private val worldData: MutableList<Point2D> = mutableListOf()
 
-    private val terrainGenerator = object : TerrainGenerator() {
+    // TODO: stub
+    private val terrainGenerator = object : TerrainGenerator(
+        noiseTypeValues = listOf(tileSets.keys.first().rangeMap.keys.first()),
+        targetTypeValues = listOf(tileSets.keys.first())
+    ) {
         override val noiseFunc: (Long, Double, Double) -> Float = noise
     }
 
@@ -36,25 +40,30 @@ class ProceduralGenerator(
                 )
             }
         }
+        println("worldDataSize: ${worldData.size}")
 
         if (widthInTiles * heightInTiles != worldData.size) {
             throw IllegalStateException("Tiles count is not valid")
         }
     }
 
-    fun generateMap(
+    fun generateLayers(
         seed: Long
-    ): TileMap {
+    ): List<Layer> {
         val data = terrainGenerator.generate(
             seed, worldData
         )
 
         val resultMap = hashMapOf<TileSet, NormalizedData>()
 
-        // TODO: move to separate method
+        /**
+         * Each element type (e.g. biome) refers to a collection of noise values and a tileset.
+         * Former is being normalized for latter.
+         */
         data.keys.forEach {
             val targetSet = tileSets[it]
             data[it]?.let { proceduralData ->
+                println("procedural data: ${proceduralData.size}")
                 val normalizedValues = normalizeForTileSet(proceduralData, targetSet!!)
                 resultMap[targetSet] = normalizedValues
             }
@@ -79,32 +88,39 @@ class ProceduralGenerator(
             )
         }.toMutableList()
 
-        return TileMap(
-            layers = layers,
-            widthInTiles = widthInTiles,
-            heightInTiles = heightInTiles
-        )
+        return layers
     }
 
 
+    /**
+     * This method maps noise values to tileset tiles' ids.
+     * Firstly it creates a list of available ids. After that each noise value is mapped to a random id.
+     *
+     * @param values is a list of points mapped to noise values, where each point is unique and refers to the start
+     * (top-left corner position) of the tile.
+     * @param set is a target tileset, which will be used as a source for rendering a layer.
+     */
     private fun normalizeForTileSet(values: List<Pair<Point2D, Float>>, set: TileSet): NormalizedData {
         val count = set.getUniqueTilesCount()
-        val randomSet = mutableSetOf<Int>()
-
-        while (randomSet.size < count) {
-            randomSet.add(Random.nextInt(count))
-        }
+        val setOfIds = (0..count).toList()
 
         val valueMap = hashMapOf<Float, Pair<Point2D, Int>>()
+        val result: NormalizedData = mutableListOf()
 
-        randomSet.forEachIndexed { index, it ->
-            val noisedValue = values[index].second
+        values.forEach {
+            val noisedValue = it.second
             if (!valueMap.contains(noisedValue)) {
-                valueMap[noisedValue] = values[index].first to it
+                valueMap[noisedValue] = it.first to setOfIds.random()
             }
         }
 
-        return valueMap.values.toList().toMutableList()
+        for (i in values.indices) {
+            result.add(
+                valueMap[values[i].second]!!
+            )
+        }
+
+        return result
     }
 
     private fun Point2D.toTileId(): Int {
