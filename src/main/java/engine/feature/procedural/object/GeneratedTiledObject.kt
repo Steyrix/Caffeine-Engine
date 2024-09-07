09d43e38a64a17800fcb1563c8e47de1528097f0
Steyrix <steyrixio@gmail.com>
2024-09-07 16:01:27 +0400

    Implement getObjectTilesPositions method

--- src/main/java/engine/feature/procedural/object/GeneratedTiledObject.kt
+++ src/main/java/engine/feature/procedural/object/GeneratedTiledObject.kt
@@ -1,9 +1,9 @@
 package engine.feature.procedural.`object`
 
+import engine.core.geometry.Point2D
 import engine.feature.procedural.MapElementType
 import engine.feature.tiled.data.TileSet
 import engine.feature.tiled.data.layer.ObjectsLayer
-import java.awt.geom.Point2D
 
 interface GeneratedTiledObject {
 
@@ -22,7 +22,35 @@ interface GeneratedTiledObject {
     /**
      * Method for determining positions' indices of object tiles on tile map
      */
-    fun getObjectTilesPositions(startPos: Point2D): List<Int> {
-        TODO()
+    fun getObjectTilesPositions(
+        startPos: Point2D,
+        widthInTiles: Int,
+        tileSize: Float
+    ): List<Pair<Point2D, Int>> {
+        val result = mutableListOf<Pair<Point2D, Int>>()
+
+        var previousIndex = -1
+        var previousPoint = Point2D(0f, 0f)
+
+        tileIdsInSet.forEachIndexed { index, i ->
+            if (index == 0) {
+                result.add(startPos to i)
+
+                previousIndex = i
+                previousPoint = startPos
+            } else {
+                val verticalOffset = previousIndex / widthInTiles - i / widthInTiles
+
+                val horizontalOffset =
+                    (previousIndex % widthInTiles) - (i % widthInTiles) * tileSize
+
+                val x = previousPoint.x + horizontalOffset
+                val y = previousPoint.y + verticalOffset
+
+                result.add(Point2D(x, y) to i)
+            }
+        }
+
+        return result
     }
 }
