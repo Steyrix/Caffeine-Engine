8f8c42181dfd9a116644b512002260de8db189b8
Steyrix <steyrixio@gmail.com>
2024-10-11 15:40:29 +0400

    Introduce Adjacent data class to represent possible adjacent tiles

--- src/main/java/engine/feature/procedural/autotiling/AutoTile.kt
+++ src/main/java/engine/feature/procedural/autotiling/AutoTile.kt
@@ -1,9 +1,13 @@
 package engine.feature.procedural.autotiling
 
-data class AutoTile(
-    val id: Int,
+data class Adjacent(
     val northId: Int,
     val westId: Int,
     val southId: Int,
     val eastId: Int
+)
+
+data class AutoTile(
+    val id: Int,
+    val adjacent: Adjacent
 )
