f630b46c214f26c1819ae3ac6010610ddc376e77
Steyrix <steyrixio@gmail.com>
2024-10-12 16:13:45 +0400

    Implement calculation of left, right, top and bottom values

--- src/main/java/engine/feature/procedural/autotiling/Autotiler.kt
+++ src/main/java/engine/feature/procedural/autotiling/Autotiler.kt
@@ -11,6 +11,31 @@ object Autotiler {
 
         layerData.forEachIndexed { index, it ->
             var bitValue = 0
+
+            var leftValue = if (index % widthInTiles > 0) {
+                layerData[index - 1]
+            } else {
+                0
+            }
+
+            var rightValue = if (index % widthInTiles < widthInTiles) {
+                layerData[index + 1]
+            } else {
+                0
+            }
+
+            var topValue = if (index + widthInTiles < layerData.size - 1) {
+                layerData[index + widthInTiles]
+            } else {
+                0
+            }
+
+            var bottomValue = if(index - widthInTiles >= 0) {
+                layerData[index - widthInTiles]
+            } else {
+                0
+            }
+
             // TODO: calculate bit value
             // TODO: get corresponding tile
         }
