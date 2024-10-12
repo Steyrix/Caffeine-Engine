bdf24aaef633fa9f18c75585983f2ceb845da1ef
Steyrix <steyrixio@gmail.com>
2024-10-12 16:04:41 +0400

    Introduce method for assigning tiles

--- src/main/java/engine/feature/procedural/autotiling/Autotiler.kt
+++ src/main/java/engine/feature/procedural/autotiling/Autotiler.kt
@@ -2,4 +2,17 @@ package engine.feature.procedural.autotiling
 
 object Autotiler {
 
+    fun generateAutoTiledArea(
+        bitValueToId: Map<Int,Int>,
+        layerData: List<Int>,
+        widthInTiles: Int,
+        heightInTiles: Int
+    ) {
+
+        layerData.forEachIndexed { index, it ->
+            var bitValue = 0
+            // TODO: calculate bit value
+            // TODO: get corresponding tile
+        }
+    }
 }
