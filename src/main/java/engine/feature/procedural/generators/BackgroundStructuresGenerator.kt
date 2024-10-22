0f1d694ebe764228aa0c4ebf98adb359008ef1cb
Steyrix <steyrixio@gmail.com>
2024-10-22 13:59:47 +0400

    Inherit BackgroundStructuresGenerator.kt from AbstractGenerator and start implementing generation method

--- src/main/java/engine/feature/procedural/generators/BackgroundStructuresGenerator.kt
+++ src/main/java/engine/feature/procedural/generators/BackgroundStructuresGenerator.kt
@@ -2,12 +2,23 @@ package engine.feature.procedural.generators
 
 import engine.core.geometry.Point2D
 
-object BackgroundStructuresGenerator {
+object BackgroundStructuresGenerator : AbstractGenerator() {
+
+    override var noiseFunc: (Long, Double, Double) -> Float = { _, _, _ ->
+        0f
+    }
 
     fun generate(
         seed: Long,
-        worldData: List<Point2D>
+        worldData: List<Point2D>,
     ): ProceduralData {
-        TODO()
+
+        worldData.forEach {
+            val noiseValue = getNoiseForCoordinate(
+                seed, it
+            )
+        }
+
+        return mutableListOf()
     }
 }
