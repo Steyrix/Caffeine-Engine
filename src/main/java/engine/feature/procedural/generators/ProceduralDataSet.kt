3e5ed61b4b3994546bd570394295b313cf6454f7
Steyrix <steyrixio@gmail.com>
2024-10-22 13:31:31 +0400

    Add relevant parameters to ProceduralDataSet.kt

--- src/main/java/engine/feature/procedural/generators/ProceduralDataSet.kt
+++ src/main/java/engine/feature/procedural/generators/ProceduralDataSet.kt
@@ -4,5 +4,8 @@ import engine.feature.procedural.MapElementType
 import engine.feature.tiled.data.TileSet
 
 data class ProceduralDataSet(
-    val terrainData: Map<MapElementType, TileSet>
+    val terrainData: Map<MapElementType, TileSet>,
+    val unwalkableTerrainData: Map<MapElementType, TileSet>,
+    val backgroundStructuresData: List<TileSet>,
+    val objectStructuresData: List<TileSet>
 )
