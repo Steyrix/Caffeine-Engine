adfeadc06de5b01125892bb448ba5eaf5dfa31a5
Steyrix <steyrixio@gmail.com>
2024-10-22 13:54:21 +0400

    Introduce AbstractGenerator.kt abstract class

---
+++ src/main/java/engine/feature/procedural/generators/AbstractGenerator.kt
@@ -0,0 +1,19 @@
+package engine.feature.procedural.generators
+
+import engine.core.geometry.Point2D
+
+abstract class AbstractGenerator {
+
+    abstract val noiseFunc: (Long, Double, Double) -> Float
+
+    protected fun getNoiseForCoordinate(
+        seed: Long,
+        pos: Point2D
+    ): Float {
+        return noiseFunc(
+            seed,
+            pos.x.toDouble(),
+            pos.y.toDouble()
+        )
+    }
+}
