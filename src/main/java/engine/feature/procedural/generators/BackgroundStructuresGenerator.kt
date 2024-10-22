deba663b5a5d6eaf7777ec13208cad8e6a55b58f
Steyrix <steyrixio@gmail.com>
2024-10-22 13:52:24 +0400

    Introduce BackgroundStructuresGenerator.kt

---
+++ src/main/java/engine/feature/procedural/generators/BackgroundStructuresGenerator.kt
@@ -0,0 +1,13 @@
+package engine.feature.procedural.generators
+
+import engine.core.geometry.Point2D
+
+object BackgroundStructuresGenerator {
+
+    fun generate(
+        seed: Long,
+        worldData: List<Point2D>
+    ): ProceduralData {
+        TODO()
+    }
+}
