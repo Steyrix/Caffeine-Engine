583e49acd2436b3506aa9008525851d1cba50ad6
Steyrix <steyrixio@gmail.com>
2024-11-10 16:03:57 +0400

    Implement Parameterized

--- src/main/java/engine/core/render/CompositeAnimatedModel.kt
+++ src/main/java/engine/core/render/CompositeAnimatedModel.kt
@@ -2,12 +2,14 @@ package engine.core.render
 
 import engine.core.entity.CompositeEntity
 import engine.core.entity.Entity
+import engine.core.update.SetOfParameters
+import engine.core.update.update2D.Parameterized
 import engine.feature.animation.Animation
 
 
 class CompositeAnimatedModel(
     private val animatedModels: List<AnimatedModel2D>
-) : CompositeEntity(), Animated, Zleveled, Entity {
+) : CompositeEntity(), Animated, Zleveled, Entity, Parameterized<SetOfParameters> {
 
     override var zLevel: Float = animatedModels.maxOf { it.zLevel }
 
@@ -32,6 +34,12 @@ class CompositeAnimatedModel(
     override fun changeAnimationSet(new: List<Animation>) {
         animatedModels.forEach {
             it.changeAnimationSet(new)
+        }
+    }
+
+    override fun updateParameters(parameters: SetOfParameters) {
+        animatedModels.forEach {
+            it.updateParameters(parameters)
         }
     }
 }
