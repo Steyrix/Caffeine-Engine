91b08cd640a407d3e146bc5ffb34f75a5a50ff23
Steyrix <steyrixio@gmail.com>
2024-11-10 14:38:19 +0400

    Inherit Drawable from ZLeveled

--- src/main/java/engine/core/render/Drawable.kt
+++ src/main/java/engine/core/render/Drawable.kt
@@ -4,11 +4,11 @@ import engine.core.shader.Shader
 import engine.core.update.SetOfParameters
 import engine.core.update.update2D.Parameterized
 
-interface Drawable<P : SetOfParameters> : Parameterized<P> {
+interface Drawable<P : SetOfParameters> : Parameterized<P>, Zleveled {
 
     var shader: Shader?
 
-    var zLevel: Float
+    override var zLevel: Float
 
     fun draw()
 }
