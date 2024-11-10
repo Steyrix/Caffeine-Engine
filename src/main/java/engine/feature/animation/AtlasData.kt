b2247592acbde7e7d9315bf83bd1d021b1f41fb7
Steyrix <steyrixio@gmail.com>
2024-11-10 16:51:19 +0400

    Revert changes

--- src/main/java/engine/feature/animation/AtlasData.kt
+++ src/main/java/engine/feature/animation/AtlasData.kt
@@ -1,11 +1,8 @@
 package engine.feature.animation
 
-import engine.core.texture.Texture2D
-
 data class AtlasData(
     val frameWidth: Float,
     val frameHeight: Float,
     val animations: List<Animation>,
-    val texturePath: String? = null,
-    val texture: Texture2D? = null
+    val texturePath: String? = null
 )
