26a7d0407449e3c424f97520220e3a14988498de
Steyrix <steyrixio@gmail.com>
2024-11-10 16:47:57 +0400

    Add texture parameter

--- src/main/java/engine/feature/animation/AtlasData.kt
+++ src/main/java/engine/feature/animation/AtlasData.kt
@@ -1,8 +1,11 @@
 package engine.feature.animation
 
+import engine.core.texture.Texture2D
+
 data class AtlasData(
     val frameWidth: Float,
     val frameHeight: Float,
     val animations: List<Animation>,
-    val texturePath: String? = null
+    val texturePath: String? = null,
+    val texture: Texture2D? = null
 )
