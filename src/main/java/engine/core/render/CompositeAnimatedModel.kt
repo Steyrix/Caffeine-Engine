55d1a36ffff28632dc0d900bdc8fafcea5e26ab8
Steyrix <steyrixio@gmail.com>
2024-11-10 15:16:58 +0400

    Inherit CompositeAnimatedModel.kt from CompositeEntity

--- src/main/java/engine/core/render/CompositeAnimatedModel.kt
+++ src/main/java/engine/core/render/CompositeAnimatedModel.kt
@@ -1,14 +1,22 @@
 package engine.core.render
 
+import engine.core.entity.CompositeEntity
 import engine.core.entity.Entity
+import engine.core.update.Updatable
 import engine.feature.animation.Animation
 
 
 class CompositeAnimatedModel(
     private val animatedModels: List<AnimatedModel2D>
-) : Animated, Zleveled, Entity  {
+) : CompositeEntity(), Animated, Zleveled, Entity, Updatable {
 
     override var zLevel: Float = animatedModels.maxOf { it.zLevel }
+
+    init {
+        animatedModels.forEach {
+            this.addComponent(it)
+        }
+    }
 
     override fun setAnimationByKey(key: String) {
         animatedModels.forEach {
