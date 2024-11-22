96bf0253cc4edfa3b39de47e465ae008aaff4dd4
Steyrix <steyrixio@gmail.com>
2024-11-22 13:43:42 +0400

    Rework normalization method (take and convert single value)

--- src/main/java/engine/feature/procedural/generators/GeneratorUtil.kt
+++ src/main/java/engine/feature/procedural/generators/GeneratorUtil.kt
@@ -5,37 +5,6 @@ import engine.feature.tiled.data.TileSet
 
 object GeneratorUtil {
 
-    /**
-     * This method maps noise values to tileset tiles' ids.
-     * Firstly it creates a list of available ids. After that each noise value is mapped to a random id.
-     *
-     * @param values is a list of points mapped to noise values, where each point is unique and refers to the start
-     * (top-left corner position) of the tile.
-     * @param set is a target tileset, which will be used as a source for rendering a layer.
-     */
-    fun normalizeForTileSet(values: List<Pair<Point2D, Float>>, set: TileSet): NormalizedData {
-        val count = set.getUniqueTilesCount()
-        val setOfIds = (0 until count).toList()
-
-        val valueMap = hashMapOf<Float, Pair<Point2D, Int>>()
-        val result: NormalizedData = mutableListOf()
-
-        values.forEach {
-            val noisedValue = it.second
-            if (!valueMap.contains(noisedValue)) {
-                valueMap[noisedValue] = it.first to setOfIds.random()
-            }
-        }
-
-        for (i in values.indices) {
-            result.add(
-                valueMap[values[i].second]!!
-            )
-        }
-
-        return result
-    }
-
     fun Point2D.toTileId(
         worldData: List<Point2D>
     ): Int {
@@ -46,5 +15,16 @@ object GeneratorUtil {
         }
 
         return 0
+    }
+
+    fun normalizeForTileSet(value: Pair<Point2D, Float?>, set: TileSet): Pair<Point2D, Int> {
+        val count = set.getUniqueTilesCount()
+        val setOfIds = (0 until count).toList()
+
+        return if (value.second == null) {
+            value.first to -1
+        } else {
+            value.first to setOfIds.random()
+        }
     }
 }
