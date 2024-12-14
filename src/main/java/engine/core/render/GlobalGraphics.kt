2313d0373b3f06885aaea1a2cadad7e7480b4091
Steyrix <steyrixio@gmail.com>
2024-12-14 15:17:18 +0400

    Implement utility method for easy creation

--- src/main/java/engine/core/render/GlobalGraphics.kt
+++ src/main/java/engine/core/render/GlobalGraphics.kt
@@ -1,21 +1,29 @@
 package engine.core.render
 
-import engine.core.render.shader.Shader
+import engine.core.render.model.AnimatedModel2D
 import engine.core.render.shader.ShaderLoader
 import engine.core.render.shader.ShaderResource
+import engine.core.texture.Texture2D
+import engine.feature.animation.AtlasData
 
 object GlobalGraphics {
 
     var animationShaderResource: ShaderResource = ShaderResource("", "")
 
-    val defaultAnimationShader: Shader
-    get() {
+    fun createAnimatedModel(
+        texturePath: String,
+        atlasData: AtlasData
+    ): AnimatedModel2D {
         if (isEmpty(animationShaderResource)) throw IllegalStateException("Animation shader is not initialized")
+        val targetShader = ShaderLoader.loadFromFile(animationShaderResource)
+        val texture = Texture2D.createInstance(texturePath)
 
-        return ShaderLoader.loadFromFile(
-            animationShaderResource.vertexShaderPath,
-            animationShaderResource.fragmentShaderPath
-        )
+        return AnimatedModel2D(
+            atlasData,
+            texture,
+        ).apply {
+            shader = targetShader
+        }
     }
 
     private fun isEmpty(resource: ShaderResource): Boolean {
